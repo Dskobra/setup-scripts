77d568d33f12a8608ed646c508e6d0f2ac3753c9
Dskobra <jbttoms@gmail.com>
2021-12-08 05:48:06 -0600

    Update coding_tools.sh
    Added github desktop repo for fedora.

--- shared/coding_tools.sh
+++ shared/coding_tools.sh
@@ -5,14 +5,17 @@ GDLINK=https://github.com/shiftkey/desktop/releases/download/release-2.9.4-linux
 GDBINARY=GitHubDesktop-linux-2.9.4-linux1.rpm
 
 coding_tools(){
+	sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
+	sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
 	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 	source ~/.bashrc
 	nvm install lts/*
 	sudo dnf groupinstall -y "Development Tools"
 	sudo dnf groupinstall -y "C Development Tools and Libraries"
 	sudo dnf groupinstall -y "RPM Development Tools"
-	sudo dnf install -y python3-tools python3-devel
+	sudo dnf install -y python3-tools python3-devel github-desktop
 	sudo dnf install -y java-11-openjdk-devel
+	#sudo dnf install -y java-17-openjdk-devel fedora 35 plans to default to this so install sdks.
 
 }
 
@@ -24,4 +27,3 @@ github_desktop(){
 }
 
 coding_tools
-github_desktop
