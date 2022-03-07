7f39739ea427eb9c816789ce5096e38175d9e880
Jordan Bottoms <jbttoms@gmail.com>
2022-03-07 03:33:22 -0600

    Update gnome.sh

--- install/de/gnome.sh
+++ install/de/gnome.sh
@@ -3,8 +3,8 @@
 gnome(){
 	echo "Now setting up some extra gnome features."
 	sudo dnf install -y menulibre pavucontrol \
-	gnome-tweaks nautilus-dropbox openssl \
-	humanity-icon-theme bluecurve-icon-theme
+	gnome-tweaks nautilus-dropbox file-roller \
+	openssl humanity-icon-theme bluecurve-icon-theme
 	flatpak install -y flathub org.gnome.Extensions
 }
 gnome
