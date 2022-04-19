a7fbcbf616f5c4289173160ae9363a5c98a5d80c
Jordan Bottoms <jbttoms@gmail.com>
2022-04-19 00:08:33 -0500

    Code cleanup
    Some code cleanup

--- install/wowapps.sh
+++ install/wowapps.sh
@@ -17,5 +17,15 @@ weakauras(){
     sudo rpm -i $WAAPBINARY
     rm $WAAPBINARY
 }
+
+raiderio(){
+    # due to the cdn they use I can't download the appimage directly.
+    mkdir /home/$USER/Apps/raiderio
+    firefox https://raider.io/addon
+    cd /home/$USER/Downloads
+    chmod +x RaiderIO_Client.AppImage
+    mv RaiderIO_Client.AppImage /home/$USER/Apps/raiderio/
+}
 wowup
 weakauras
+raiderio
