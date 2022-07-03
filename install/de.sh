359b4d5f3337e8a7e6e7f95f272ac7dabe5344c0
Jordan Bottoms <jbttoms@gmail.com>
2022-07-03 08:40:32 -0500

    Improved code for determining desktop
    Now checks XDG_CURRENT_DESKTOP variable for desktop session instead of using test to look for session managers. Also renamed world of warcraft mangohud conf file to proper wow executable name and customized the guild wars 2 conf file

--- install/de.sh
+++ install/de.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/bash
 
 get_desktop_extras(){
-test -f /usr/bin/gnome-session && DESKTOP=gnome
-test -f /usr/bin/plasma_session && DESKTOP=kde
-test -f /usr/bin/mate-session && DESKTOP=mate
-
-	if [ "$DESKTOP" = "gnome" ];
+DESKTOP=$XDG_CURRENT_DESKTOP
+	if [ "$DESKTOP" = "GNOME" ];
 	then
 		./install/de/gnome.sh
-	elif [ "$DESKTOP" = "kde" ];
+	elif [ "$DESKTOP" = "KDE" ];
 	then
 		./install/de/kde.sh
-	elif [ "$DESKTOP" = "mate" ];
+	elif [ "$DESKTOP" = "MATE" ];
 	then
 		./install/de/mate.sh
 	fi
 }
-DESKTOP=0
 get_desktop_extras
