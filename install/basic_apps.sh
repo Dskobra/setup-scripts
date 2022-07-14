e433ab7ce760f150c016a2ec6b4b483beff301f7
Jordan Bottoms <jbttoms@gmail.com>
2022-07-14 00:13:35 -0500

    Update basic_apps.sh
    Moved vim-enhanced back tot he basic_apps script (didnt mean to move it :P )

--- install/basic_apps.sh
+++ install/basic_apps.sh
@@ -2,8 +2,8 @@
 
 basic_apps(){
 	sudo dnf install -y  java-17-openjdk brave-browser \
-	plymouth-theme-spinfinity  \
-	bluecurve-icon-theme lm_sensors \
+	plymouth-theme-spinfinity vim-enhanced lm_sensors \
+	bluecurve-icon-theme
 	sudo plymouth-set-default-theme spinfinity -R
 	flatpak install -y flathub org.keepassxc.KeePassXC
 	sudo mkdir /opt/launchers
