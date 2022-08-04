f6b7f51f75c084cdce6660c96d1bac1569843f9e
Jordan Bottoms <jbttoms@gmail.com>
2022-08-04 01:42:17 -0500

    Update deck.sh

--- install/deck.sh
+++ install/deck.sh
@@ -5,4 +5,6 @@ sudo pacman -R firefox
 flatpak install -y flathub net.davidotek.pupgui2
 flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub com.dropbox.Client
+flatpak install -y flathub org.keepassxc.KeePassXC
+flatpak install -y flathub com.discordapp.Discord
 flatpak install flathub org.gnome.Platform.Compat.i386 org.freedesktop.Platform.GL32.default org.freedesktop.Platform.GL.default
