8a7cac05bb2f041311dad53ddef79719ab434dd8
Jordan Bottoms <jbttoms@gmail.com>
2022-08-12 17:21:05 -0500

    Update wowup.sh
    Updated wowup link

--- install/wowup.sh
+++ install/wowup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-WOWUPLINK=https://github.com/WowUp/WowUp/releases/download/v2.8.2/WowUp-2.8.2.AppImage
-WOWUPBINARY=WowUp-2.8.2.AppImage
+WOWUPLINK=https://github.com/WowUp/WowUp/releases/download/v2.8.3/WowUp-2.8.3.AppImage
+WOWUPBINARY=WowUp-2.8.3.AppImage
 
 
 wowup(){
