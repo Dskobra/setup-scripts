ba6b24b3dc92642a8627cc0215f968e29d209872
Jordan Bottoms <jbttoms@gmail.com>
2022-10-10 23:25:20 -0500

    Update setup.sh
    Finished moving stuff over to main script

--- setup.sh
+++ setup.sh
@@ -91,6 +91,89 @@ install_utilities(){
 	flatpak install -y flathub org.gtkhash.gtkhash
 	flatpak install -y flathub com.github.tchx84.Flatseal
 }
+
+install_bottles(){
+	mkdir /home/$USER/Games
+	mkdir /home/$USER/Games/bottles
+	flatpak install -y flathub com.usebottles.bottles
+}
+
+install_lutris(){
+	sudo dnf install -y gnome-desktop3 xrandr xorg-x11-server-Xephyr python3-evdev gvfs cabextract \
+	python3-magic libraqm python3-olefile python3-pillow fluid-soundfont-gs p7zip p7zip-plugins 
+	sudo dnf install -y lutris
+}
+
+install_wine(){
+	echo "Installing wine and various libs for things like wow."
+    sudo dnf install -y alsa-plugins-pulseaudio.i686 glibc-devel.i686 glibc-devel libgcc.i686 libX11-devel.i686 freetype-devel.i686
+    sudo dnf install -y libXcursor-devel.i686 libXi-devel.i686 libXext-devel.i686 libXxf86vm-devel.i686 libXrandr-devel.i686
+    sudo dnf install -y libXinerama-devel.i686 mesa-libGLU-devel.i686 mesa-libOSMesa-devel.i686 libXrender-devel.i686 libpcap-devel.i686
+    sudo dnf install -y ncurses-devel.i686 libzip-devel.i686 lcms2-devel.i686 zlib-devel.i686 libv4l-devel.i686 libgphoto2-devel.i686
+    sudo dnf install -y cups-devel.i686 libxml2-devel.i686 openldap-devel.i686 libxslt-devel.i686 gnutls-devel.i686 libpng-devel.i686
+    sudo dnf install -y flac-libs.i686 json-c.i686 libICE.i686 libSM.i686 libXtst.i686 libasyncns.i686 libedit.i686
+    sudo dnf install -y liberation-narrow-fonts.noarch libieee1284.i686 libogg.i686 libsndfile.i686 libuuid.i686 libva.i686 libvorbis.i686
+    sudo dnf install -y libwayland-client.i686 libwayland-server.i686 llvm-libs.i686 mesa-dri-drivers.i686 mesa-filesystem.i686 mesa-libEGL.i686
+    sudo dnf install -y mesa-libgbm.i686 nss-mdns.i686 ocl-icd.i686 pulseaudio-libs.i686  sane-backends-libs.i686 tcp_wrappers-libs.i686 unixODBC.i686
+    sudo dnf install -y samba-common-tools.x86_64 samba-libs.x86_64 samba-winbind.x86_64
+    sudo dnf install -y samba-winbind-clients.x86_64 samba-winbind-modules.x86_64 mesa-libGL-devel.i686 fontconfig-devel.i686 libXcomposite-devel.i686
+    sudo dnf install -y libtiff-devel.i686 openal-soft-devel.i686 mesa-libOpenCL-devel.i686 opencl-utils-devel.i686 alsa-lib-devel.i686 gsm-devel.i686
+    sudo dnf install -y libjpeg-turbo-devel.i686 pulseaudio-libs-devel.i686 pulseaudio-libs-devel gtk3-devel.i686 libattr-devel.i686 libva-devel.i686
+    sudo dnf install -y libexif-devel.i686 libexif.i686 glib2-devel.i686 mpg123-devel.i686 mpg123-devel.x86_64 libcom_err-devel.i686
+    sudo dnf install -y libcom_err-devel.x86_64 libFAudio-devel.i686 libFAudio-devel.x86_64
+    sudo dnf install -y gnutls.i686 gnutls-devel openldap openldap.i686
+	sudo dnf install -y openldap-devel libgpg-error libgpg-error.i686 sqlite2.i686 sqlite2.x86_64
+	sudo dnf install -y pipewire-devel.i686 pipewire-devel.x86_64 gstreamer1*.i686 gstreamer1*.x86_64
+	sudo dnf install -y freetype.i686 gnutls.i686 openldap.i686 libgpg-error.i686 sqlite2.i686 pulseaudio-libs.i686
+	sudo dnf install -y dxvk-native-devel.x86_64 dxvk-native-devel.i686 vulkan-tools
+    sudo dnf install -y winehq-staging
+}
+
+install_wowup(){
+    WOWUPLINK=https://github.com/WowUp/WowUp/releases/download/v2.9.0/WowUp-2.9.0.AppImage
+    WOWUPBINARY=WowUp-2.9.0.AppImage
+    mkdir /home/$USER/Downloads/wowup 
+    cd /home/$USER/Downloads/wowup 
+    wget $WOWUPLINK
+    chmod +x $WOWUPBINARY
+    sudo mv /home/$USER/Downloads/wowup /opt/wowup
+}
+
+setup_wine_repo(){
+	source /etc/os-release
+	getRelease=$(echo $VERSION_ID)
+	echo "Fedora Version:" $getRelease
+
+	if [ "$getRelease" = "36" ]
+	then
+		# remove after 37 is released, winehq updated and no major bugs.
+		sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo
+	elif [ "$getRelease" = "37" ]
+	then
+		# temporarily use fedora 36 winehq repo on fedora 37. change when official 37 support is added.
+		sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo
+	elif [ "$getRelease" = "38" ]
+	then
+		# temporarily use fedora 36 winehq repo on fedora 37. change when official 37 support is added.
+		#sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo
+		echo "Fedora 38 placeholder. Not due for release until around may/june"
+	elif [ $input -eq 0 ]
+	then
+		exit
+	else
+		echo "error."
+	fi
+}
+
+install_samba(){
+	sudo dnf install -y samba
+	sudo systemctl enable smb nmb
+	sudo firewall-cmd --add-service=samba --permanent
+	sudo firewall-cmd --reload
+	sudo mkdir /mnt/shared
+	sudo chown $USER /mnt/shared -R
+}
+
 main_help(){
     echo "1. Repos - rpmfusion, flatpak and brave browser."
     echo "2. Amd Drivers - x11 driver arnd radeon-profile package for fan control. Doesnt work on laptops or cards without fans."
@@ -134,13 +217,15 @@ games_menu(){
 	    flatpak install -y flathub net.davidotek.pupgui2
     elif [ $input -eq 2 ]
     then
-        ./install/winehq.sh
+        setup_wine_repo
+        install_wine
     elif [ $input -eq 3 ]
     then
-        ./install/gaming_apps.sh
+        install_bottles
+        install_lutris
     elif [ $input -eq 4 ]
     then
-        ./install/wowup.sh
+        install_wowup
     elif [ $input -eq 5 ]
     then
         flatpak -y install flathub com.mojang.Minecraft
@@ -210,8 +295,8 @@ office_help(){
 
 servers_menu(){
     echo "================================================"
-    echo "1. Lamp Stack 2. Samba Share"
-    echo "3. Fedora Cockpit"
+    echo "1. Lamp Stack 2. Fedora Cockpit"
+    echo "3. Samba Share"
     echo "99. Help 0. Back to main menu"
     echo "================================================"
     printf "Option: "
@@ -230,7 +315,7 @@ servers_menu(){
 	    sudo firewall-cmd --add-service=cockpit --permanent
     elif [ $input -eq 3 ]
     then
-        ./install/samba_share.sh
+        install_samba
     elif [ $input -eq 99 ]
     then
         servers_help
