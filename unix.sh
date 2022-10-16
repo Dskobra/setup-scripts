18d5c5e3176a877d3616004a3569be35264afc96
Jordan Bottoms <jbttoms@gmail.com>
2022-10-16 02:42:16 -0500

    Update unix.sh
    Redid the code to be cleaner

--- unix.sh
+++ unix.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/bash
 #fix encoding issues. I made this on windows and it keps defaulting to non unix line endings.
 
+#dos2unix *
+#cd custom
+#dos2unix *
+#cd data
+#dos2unix *
+#cd mangohud
+#dos2unix *
+#cd ../shortcuts
+#dos2unix *
+
+PARENT=$(pwd)
 dos2unix *
-cd custom
+cd $PARENT/custom/data
 dos2unix *
-cd data
+cd $PARENT/custom/data/mangohud
 dos2unix *
-cd mangohud
-dos2unix *
-cd ../shortcuts
+cd $PARENT/custom/data/shortcuts
 dos2unix *
