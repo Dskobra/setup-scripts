bbcee0da2be8de72ae99686f4cbfbc17ae8d931b
Jordan Bottoms <jbttoms@gmail.com>
2023-05-25 17:46:29 -0500

    Update setup.sh
    Reorganized functions and removed some older ones

--- setup.sh
+++ setup.sh
@@ -40,6 +40,80 @@ main_menu(){
     main_menu
 }
 
+install_basic_apps(){
+	echo "Setting up rpmfusion and flathub."
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	if [ "$DESKTOP" = "MATE" ]
+	then
+		echo "Now setting up some extra mate features."
+		sudo dnf install -y caja-share mate-menu \
+        dconf-editor humanity-icon-theme \
+        gnome-icon-theme pavucontrol
+    else
+        echo "Not running Mate."
+	fi
+	sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+	sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+	sudo dnf update -y
+
+
+	sudo dnf install -y  java-17-openjdk brave-browser \
+	plymouth-theme-spinfinity vim-enhanced lm_sensors \
+    p7zip p7zip-plugins hplip-gui dnfdragora             
+    
+    sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+    sudo dnf install -y gstreamer1-plugin-openh264 \
+	mozilla-openh264 ffmpeg ffmpeg-libs.i686 ffmpeg-libs
+    sudo dnf groupinstall -y "Firefox Web Browser"
+	sudo plymouth-set-default-theme spinfinity -R
+    
+	flatpak install -y flathub org.keepassxc.KeePassXC
+    flatpak install -y flathub com.transmissionbt.Transmission
+    flatpak install -y flathub com.dropbox.Client
+
+    mkdir "$HOME"/.config/autostart # some desktops like mate dont have this created by default.
+
+
+}
+
+install_game_clients(){
+    mkdir "$HOME"/Games
+	mkdir "$HOME"/Games/bottles
+    mkdir "$HOME"/.config/MangoHud/
+    sudo dnf install -y steam goverlay
+    sudo modprobe xpad
+	flatpak install -y flathub net.davidotek.pupgui2
+    flatpak install -y runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/21.08
+    flatpak install -y runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/22.08
+
+    flatpak install -y flathub com.usebottles.bottles
+    flatpak install -y flathub net.lutris.Lutris 
+    
+    # run once to ensure folders/runtimes are setup
+    flatpak run com.usebottles.bottles
+    flatpak run net.lutris.Lutris
+
+    cd "$HOME"/Downloads
+    wget https://launcher.mojang.com/download/Minecraft.tar.gz
+    tar -xvf Minecraft.tar.gz
+    cd minecraft-launcher
+    chmod +x minecraft-launcher
+    mv minecraft-launcher "$HOME"/Desktop
+    cd "$HOME"/Downloads
+    rm -r minecraft-launcher
+    rm Minecraft.tar.gz
+
+    flatpak install -y flathub org.kde.kpat
+
+    WOWUPLINK=https://github.com/WowUp/WowUp.CF/releases/download/v2.9.2/WowUp-CF-2.9.2.AppImage
+    WOWUPBINARY=WowUp-CF-2.9.2.AppImage
+    cd "$HOME"/Desktop
+    wget $WOWUPLINK
+    chmod +x $WOWUPBINARY
+    
+}
+
 coding_servers_menu(){
     echo "================================================"
     echo "Coding and Servers"
@@ -81,6 +155,22 @@ coding_servers_menu(){
     echo $input
     unset input
     coding_servers_menu
+}
+
+install_coding_tools(){
+	sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+    sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+	printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
+	sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
+    sudo dnf groupinstall -y "C Development Tools and libraries"
+    sudo dnf groupinstall -y "Development Tools"
+    sudo dnf groupinstall -y "RPM Development Tools"
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+	source ~/.bashrc
+	nvm install lts/*
+	sudo dnf install -y python3-idle python3-devel git-gui \
+	java-17-openjdk-devel codium github-desktop
+
 }
 
 extras_menu(){
@@ -143,111 +233,6 @@ extra_apps(){
 
 }
 
-install_basic_apps(){
-	echo "Setting up rpmfusion and flathub."
-    flatpak remote-modify --disable fedora
-    if [ "$fedoraVersion" = "38" ]
-    then
-        #echo "Fedora Version: $fedoraVersion includes flathub, but disabled by default."
-        #flatpak remote-modify --enable flathub
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	elif [ "$fedoraVersion" = "37" ]
-	then
-		echo "Fedora Version: $fedoraVersion which requires adding remote flathub."
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    else
-        echo "Unknown error has occured"
-	fi
-	sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-	sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-	sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-	sudo dnf update -y
-
-
-	sudo dnf install -y  java-17-openjdk brave-browser \
-	plymouth-theme-spinfinity vim-enhanced lm_sensors \
-    p7zip p7zip-plugins hplip-gui dnfdragora             
-    
-    sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
-    sudo dnf install -y gstreamer1-plugin-openh264 \
-	mozilla-openh264 ffmpeg ffmpeg-libs.i686 ffmpeg-libs
-    sudo dnf groupinstall -y "Firefox Web Browser"
-	sudo plymouth-set-default-theme spinfinity -R
-    
-	flatpak install -y flathub org.keepassxc.KeePassXC
-    flatpak install -y flathub com.transmissionbt.Transmission
-    flatpak install -y flathub com.dropbox.Client
-
-    mkdir "$HOME"/.config/autostart # some desktops like mate dont have this created by default.
-
-
-}
-
-get_desktop_extras(){
-	if [ "$DESKTOP" = "MATE" ]
-	then
-		echo "Now setting up some extra mate features."
-		sudo dnf install -y caja-share mate-menu \
-        dconf-editor humanity-icon-theme \
-        gnome-icon-theme pavucontrol
-    else
-        echo "Not running Mate."
-	fi
-}
-
-install_coding_tools(){
-	sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-    sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
-	printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
-	sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
-    sudo dnf groupinstall -y "C Development Tools and libraries"
-    sudo dnf groupinstall -y "Development Tools"
-    sudo dnf groupinstall -y "RPM Development Tools"
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-	source ~/.bashrc
-	nvm install lts/*
-	sudo dnf install -y python3-idle python3-devel git-gui \
-	java-17-openjdk-devel codium github-desktop
-
-}
-
-install_game_clients(){
-    mkdir "$HOME"/Games
-	mkdir "$HOME"/Games/bottles
-    mkdir "$HOME"/.config/MangoHud/
-    sudo dnf install -y steam goverlay
-    sudo modprobe xpad
-	flatpak install -y flathub net.davidotek.pupgui2
-    flatpak install -y runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/21.08
-    flatpak install -y runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/22.08
-
-    flatpak install -y flathub com.usebottles.bottles
-    flatpak install -y flathub net.lutris.Lutris 
-    
-    # run once to ensure folders/runtimes are setup
-    flatpak run com.usebottles.bottles
-    flatpak run net.lutris.Lutris
-
-    cd "$HOME"/Downloads
-    wget https://launcher.mojang.com/download/Minecraft.tar.gz
-    tar -xvf Minecraft.tar.gz
-    cd minecraft-launcher
-    chmod +x minecraft-launcher
-    mv minecraft-launcher "$HOME"/Desktop
-    cd "$HOME"/Downloads
-    rm -r minecraft-launcher
-    rm Minecraft.tar.gz
-
-    flatpak install -y flathub org.kde.kpat
-
-    WOWUPLINK=https://github.com/WowUp/WowUp.CF/releases/download/v2.9.2/WowUp-CF-2.9.2.AppImage
-    WOWUPBINARY=WowUp-CF-2.9.2.AppImage
-    cd "$HOME"/Desktop
-    wget $WOWUPLINK
-    chmod +x $WOWUPBINARY
-    
-}
-
 mango(){
     # link bottles/lutris to mangohud configuration folder
     ln -s "$HOME/.config/MangoHud/" "$HOME/.var/app/com.usebottles.bottles/config/"
@@ -256,12 +241,6 @@ mango(){
     ./setup.sh
 
     
-}
-
-get_fedora_version(){
-    source /etc/os-release
-	fedoraVersion=$(echo $VERSION_ID)
-	echo "Fedora Version:" $fedoraVersion
 }
 
 cleanup(){
@@ -289,5 +268,4 @@ about(){
 
 SCRIPTS_HOME=$(pwd)
 DESKTOP=$XDG_CURRENT_DESKTOP
-$fedoraVersion
 main_menu
