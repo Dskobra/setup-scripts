c8e2a12321fa82b8b8bcfff3c54b4345fd21bd67
Jordan Bottoms <jbttoms@gmail.com>
2023-05-26 08:15:41 -0500

    Update setup.sh
    cleaned up leftover line

--- setup.sh
+++ setup.sh
@@ -39,8 +39,6 @@ main_menu(){
 }
 
 install_basic_apps(){
-	echo "Setting up rpmfusion and flathub."
-    bash -c "source flatpak.sh; fbasic"
 	sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 	sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 	sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
