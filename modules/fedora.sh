008aa99b7ecd2a0b7647f2d2d440a7de743e75be
Jordan Bottoms <jbttoms@gmail.com>
2023-05-29 00:05:39 -0500

    Update fedora.sh
    updated echo text

--- modules/fedora.sh
+++ modules/fedora.sh
@@ -39,6 +39,7 @@ main_menu(){
 }
 
 install_basic_apps(){
+    echo "Setting up rpmfusion and brave browser"
 	sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 	sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 	sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
