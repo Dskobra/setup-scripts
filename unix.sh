3d74cb12877ebdbcf9be6e36a819ac5a91ea2b91
Jordan Bottoms <jbttoms@gmail.com>
2023-06-09 06:09:57 -0500

    updated dos2unix commands
    Forgot to update the folders in unix.sh that dos2unix runs on

--- unix.sh
+++ unix.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 #fix encoding issues. I made this on windows and it keps defaulting to non unix line endings.
 
-PARENT=$(pwd)
+SCRIPTS_HOME=$(pwd)
 dos2unix *
-cd $PARENT/mangohud
+cd $SCRIPTS_HOME/modules
+dos2unix *
+cd $SCRIPTS_HOME/modules/mango_conf
 dos2unix *
