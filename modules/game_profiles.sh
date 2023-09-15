915640f79bc8672c8e29bb327201df15008cf4ec
Jordan Bottoms <jbttoms@gmail.com>
2023-09-15 00:34:00 -0500

    renamed mangohud
    Renamed mangohud to game_profiles

--- modules/game_profiles.sh
+++ modules/game_profiles.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 cd $SCRIPTS_HOME/modules/
-git clone https://github.com/Dskobra/setup-scripts -b mangohud
+git clone https://github.com/Dskobra/setup-scripts -b game_profiles
 
-mv setup-scripts mango_conf
-cd mango_conf
+mv setup-scripts game_profiles
+cd game_profiles
 dos2unix *.conf
 sudo chown $USER:$USER *.conf
 cp *.conf $HOME/.config/MangoHud/
