b1418c4d4ca8c051a826ed344cb81c5f61336fbb
Jordan Bottoms <jbttoms@gmail.com>
2023-09-28 20:02:39 -0500

    Update dev_tools.sh
    Removed unneeded package

--- modules/dev_tools.sh
+++ modules/dev_tools.sh
@@ -86,8 +86,7 @@ install_mugshot(){
     curl -L -o $MUGSHOT_FOLDER.tar.gz https://github.com/bluesabre/mugshot/releases/download/mugshot-0.4.3/mugshot-0.4.3.tar.gz
     tar -xvf $MUGSHOT_FOLDER.tar.gz
     cd $MUGSHOT_FOLDER
-    sudo dnf install -y typelib-1_0-Retro-1-0 clutter-gtk gst-devtools\
-    python3-gobject gdk-pixbuf2 python3-distutils-extra
+    sudo dnf install -y clutter-gtk gst-devtools python3-gobject gdk-pixbuf2 python3-distutils-extra
     sudo python3 setup.py install
     sudo mkdir /usr/local/share/glib-2.0/schemas
     cd data/glib-2.0/schemas/
