d4c49fe16a021676d0d578a000e94f0db1ec342e
Jordan Bottoms <jbttoms@gmail.com>
2023-09-28 19:54:00 -0500

    Merged mugshot code
    Merged mugshot setup code into dev_tools.

--- modules/dev_tools.sh
+++ modules/dev_tools.sh
@@ -14,6 +14,7 @@ dev_menu(){
         1)
         install_vm_basic_apps
         install_vm_dev_tools
+        install_mugshot
         ;;
 
         2)
@@ -77,6 +78,22 @@ install_vm_dev_tools(){
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 	source ~/.bashrc
 	nvm install lts/*
+}
+
+install_mugshot(){
+    MUGSHOT_FOLDER="mugshot-0.4.3"
+    cd $SCRIPTS_HOME/modules/
+    curl -L -o $MUGSHOT_FOLDER.tar.gz https://github.com/bluesabre/mugshot/releases/download/mugshot-0.4.3/mugshot-0.4.3.tar.gz
+    tar -xvf $MUGSHOT_FOLDER.tar.gz
+    cd $MUGSHOT_FOLDER
+    sudo dnf install -y typelib-1_0-Retro-1-0 clutter-gtk gst-devtools\
+    python3-gobject gdkpixbuf2 python3-distutils-extra
+    sudo python3 setup.py install
+    sudo mkdir /usr/local/share/glib-2.0/schemas
+    cd data/glib-2.0/schemas/
+    sudo cp org.bluesabre.mugshot.gschema.xml  /usr/local/share/glib-2.0/schemas
+    sudo glib-compile-schemas /usr/local/share/glib-2.0/schemas
+
 }
 
 container_dev_tools(){
