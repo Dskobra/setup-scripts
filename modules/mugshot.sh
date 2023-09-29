d52e6a079cff51ad2bd7cf82c36fb15da300abf6
Jordan Bottoms <jbttoms@gmail.com>
2023-09-28 19:39:32 -0500

    Update mugshot.sh
    Added extraction command

--- modules/mugshot.sh
+++ modules/mugshot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 MUGSHOT_FOLDER="mugshot-0.4.3"
 curl -L -o $MUGSHOT_FOLDER.tar.gz https://github.com/bluesabre/mugshot/releases/download/mugshot-0.4.3/mugshot-0.4.3.tar.gz
+tar -xvf $MUGSHOT_FOLDER.tar.gz
 cd $MUGSHOT_FOLDER
 sudo dnf install typelib-1_0-Retro-1-0 clutter-gtk gst-devtools python3-gobject gdkpixbuf2
 sudo python3 setup.py install
