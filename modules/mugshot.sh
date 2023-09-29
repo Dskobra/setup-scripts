3ea84c4eee18c480aecd52376c330c5e4a323102
Jordan Bottoms <jbttoms@gmail.com>
2023-09-28 19:20:25 -0500

    Create mugshot.sh
    added script to download and build mugshot for xfce profile pictures

---
+++ modules/mugshot.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+MUGSHOT_FOLDER="mugshot-0.4.3"
+curl -L -o $MUGSHOT_FOLDER.tar.gz https://github.com/bluesabre/mugshot/releases/download/mugshot-0.4.3/mugshot-0.4.3.tar.gz
+cd $MUGSHOT_FOLDER
+sudo dnf install typelib-1_0-Retro-1-0 clutter-gtk gst-devtools python3-gobject gdkpixbuf2
+sudo python3 setup.py install
+sudo mkdir /usr/local/share/glib-2.0/schemas
+cd data/glib-2.0/schemas/
+sudo cp org.bluesabre.mugshot.gschema.xml  /usr/local/share/glib-2.0/schemas
+sudo glib-compile-schemas /usr/local/share/glib-2.0/schemas
