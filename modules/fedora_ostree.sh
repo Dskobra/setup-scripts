e65efbdd30fe26a2c803ca79eac997e379f93673
Jordan Bottoms <jbttoms@gmail.com>
2023-10-13 01:51:10 -0500

    Update fedora_ostree.sh
    added corectrl autostart changes to ostree script

--- modules/fedora_ostree.sh
+++ modules/fedora_ostree.sh
@@ -196,8 +196,10 @@ autostart(){
     cp /home/$USER/.local/share/flatpak/exports/share/applications/com.dropbox.Client.desktop /home/$USER/.config/autostart/com.dropbox.Client.desktop
     DISCORD="/home/$USER/.local/share/flatpak/exports/share/applications/com.discordapp.Discord.desktop"
     STEAM="/usr/share/applications/steam.desktop"
+    CORECTRL="/usr/share/applications/org.corectrl.corectrl.desktop"
     [ -f $DISCORD ] && { echo "Discord was found. Adding to startup."; cp "$DISCORD"  /home/$USER/.config/autostart/com.discordapp.Discord.desktop; }
     [ -f $STEAM ] && { echo "Steam was found. Adding to startup."; cp "$STEAM"  /home/$USER/.config/autostart/steam.desktop; }
+    [ -f $CORECTRL ] && { echo "Corectrl was found. Adding to startup."; cp "$CORECTRL"  /home/$USER/.config/autostart/org.corectrl.corectrl.desktop; }
 
 }
 
