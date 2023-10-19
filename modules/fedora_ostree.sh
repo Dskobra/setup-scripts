960d617165281102be106be086e2c3104064e3d6
Jordan Bottoms <jbttoms@gmail.com>
2023-10-19 01:00:31 -0500

    Update fedora_ostree.sh
    Added xvideowaylandbridge to autostart in ostree script

--- modules/fedora_ostree.sh
+++ modules/fedora_ostree.sh
@@ -204,9 +204,13 @@ autostart(){
     DISCORD="/home/$USER/.local/share/flatpak/exports/share/applications/com.discordapp.Discord.desktop"
     STEAM="/usr/share/applications/steam.desktop"
     CORECTRL="/usr/share/applications/org.corectrl.corectrl.desktop"
+    XWVIDEO_BRIDGE="/usr/share/applications/org.kde.xwaylandvideobridge.desktop"
+
+
     [ -f $DISCORD ] && { echo "Discord was found. Adding to startup."; cp "$DISCORD"  /home/$USER/.config/autostart/com.discordapp.Discord.desktop; }
     [ -f $STEAM ] && { echo "Steam was found. Adding to startup."; cp "$STEAM"  /home/$USER/.config/autostart/steam.desktop; }
     [ -f $CORECTRL ] && { echo "Corectrl was found. Adding to startup."; cp "$CORECTRL"  /home/$USER/.config/autostart/org.corectrl.corectrl.desktop; }
+    [ -f $XWVIDEO_BRIDGE ] && { echo "XWaylandVideoBridge was found. Adding to startup."; cp "$XWVIDEO_BRIDGE"  /home/$USER/.config/autostart/org.kde.xwaylandvideobridge.desktop; }
 
 }
 
