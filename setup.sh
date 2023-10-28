2d6cfc345f317152b502394f0fffc36b29f3c3b6
Jordan Bottoms <jbttoms@gmail.com>
2023-10-28 01:03:42 -0500

    Removed ostree
    Moved ostree to experiments branch.

--- setup.sh
+++ setup.sh
@@ -9,35 +9,7 @@ launch_menu(){
     echo "     Copyright (c) 2021-2023 Jordan Bottoms"
     echo "         Released under the MIT license"
     echo ""
-    echo "                  Menu"
-    echo ""
-    echo "1. dnf                    2. ostree"
-    echo "0. Exit"
-    printf "Option: "
-    read -r input
-
-    case $input in
-
-        1)
-        $SCRIPTS_HOME/modules/dnf.sh
-        ;;
-
-        2)
-        $SCRIPTS_HOME/modules/ostree.sh
-        sudo rm -r -f $SCRIPTS_HOME/temp
-        ;;
-
-        0)
-        exit
-        ;;
-
-    *)
-        echo -n "Unknown entry"
-        echo ""
-        launch_menu
-        ;;
-    esac
-    unset input
+   $SCRIPTS_HOME/modules/dnf.sh
 }
 
 VERSION="dev branch"
