5347b5abbbb19795bbfcf5128000f1bde321a486
Jordan Bottoms <jbttoms@gmail.com>
2023-11-05 20:32:18 -0600

    Update shared.sh
    added autoconfirmation

--- modules/shared.sh
+++ modules/shared.sh
@@ -4,7 +4,7 @@ fbasic(){
     echo "Setting up flathub for user"
     flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     flatpak install --user -y flathub org.keepassxc.KeePassXC
-    flatpak install --user flathub com.bitwarden.desktop
+    flatpak install --user -y flathub com.bitwarden.desktop
     flatpak install --user -y flathub com.dropbox.Client
 
 }
