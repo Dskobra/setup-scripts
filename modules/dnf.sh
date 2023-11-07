d4d23f7cf7254e6d468ab4bc2c9285ba037f894a
Jordan Bottoms <jbttoms@gmail.com>
2023-11-07 05:39:23 -0600

    Update dnf.sh
    added goverlay to startup

--- modules/dnf.sh
+++ modules/dnf.sh
@@ -376,14 +376,17 @@ autostart(){
     mkdir "$HOME"/.config/autostart # some desktops like mate dont have this created by default.
     cp /home/$USER/.local/share/flatpak/exports/share/applications/com.dropbox.Client.desktop /home/$USER/.config/autostart/com.dropbox.Client.desktop
     DISCORD="/home/$USER/.local/share/flatpak/exports/share/applications/com.discordapp.Discord.desktop"
+    GOVERLAY="/home/$USER/.local/share/flatpak/exports/share/applications/io.github.trigg.discover_overlay"
     STEAM="/usr/share/applications/steam.desktop"
     CORECTRL="/usr/share/applications/org.corectrl.corectrl.desktop"
     XWVIDEO_BRIDGE="/usr/share/applications/org.kde.xwaylandvideobridge.desktop"
 
     [ -f $DISCORD ] && { echo "Discord was found. Adding to startup."; cp "$DISCORD"  /home/$USER/.config/autostart/com.discordapp.Discord.desktop; }
+    [ -f $GOVERLAY ] && { echo "GOverlay was found. Adding to startup."; cp "$GOVERLAY"  /home/$USER/.config/autostart/io.github.trigg.discover_overlay }
     [ -f $STEAM ] && { echo "Steam was found. Adding to startup."; cp "$STEAM"  /home/$USER/.config/autostart/steam.desktop; }
     [ -f $CORECTRL ] && { echo "Corectrl was found. Adding to startup."; cp "$CORECTRL"  /home/$USER/.config/autostart/org.corectrl.corectrl.desktop; }
     [ -f $XWVIDEO_BRIDGE ] && { echo "XWaylandVideoBridge was found. Adding to startup."; cp "$XWVIDEO_BRIDGE"  /home/$USER/.config/autostart/org.kde.xwaylandvideobridge.desktop; }
+
 }
 
 cleanup(){
