e21accbecedac222881587e35c82f3b520740e00
Jordan Bottoms <jbttoms@gmail.com>
2024-01-25 01:42:42 -0600

    added flatpak for asunder
    Asunder is only in packman multimedia section. I only provide essentials.

--- modules/opensuse/opensuse_packages.sh
+++ modules/opensuse/opensuse_packages.sh
@@ -97,23 +97,11 @@ install_xfcemm(){
     if [ ! -n "$VARIANT" ];
     then
         sudo $PKGMGR -n install $XFCE_MM_OPENSUSE
+        flatpak install --user -y $GTK_ASUNDER
     elif [ $VARIANT == "ostree" ];
     then
         echo "Immutable variants are unsupported"
     fi
-}
-
-install_mugshot(){
-    MUGSHOT_FOLDER="mugshot-0.4.3"
-    cd $SCRIPTS_HOME/temp/
-    curl -L -o $MUGSHOT_FOLDER.tar.gz https://github.com/bluesabre/mugshot/releases/download/mugshot-0.4.3/mugshot-0.4.3.tar.gz
-    tar -xvf $MUGSHOT_FOLDER.tar.gz
-    cd $MUGSHOT_FOLDER
-    sudo python3 setup.py install
-    sudo mkdir /usr/local/share/glib-2.0/schemas
-    cd data/glib-2.0/schemas/
-    sudo cp org.bluesabre.mugshot.gschema.xml  /usr/local/share/glib-2.0/schemas
-    sudo glib-compile-schemas /usr/local/share/glib-2.0/schemas
 }
 
 install_mate_apps(){
