c996bb00e87a8c74859fcef12e8694d148e5317e
Jordan Bottoms <jbttoms@gmail.com>
2024-01-31 00:50:35 -0600

    Testing an all in 1 menu

--- setup.sh
+++ setup.sh
@@ -16,11 +16,11 @@ distro_check(){
     DISTRO=$(source /etc/os-release ; echo $ID)
     if [ $DISTRO == "fedora" ]
     then
-        $SCRIPTS_HOME/modules/fedora/fedora.sh
-
+        $SCRIPTS_HOME/modules/menu.sh
+        fedora_variant_check
     elif [ $DISTRO == "opensuse-tumbleweed" ]
     then
-        $SCRIPTS_HOME/modules/opensuse/opensuse.sh
+        $SCRIPTS_HOME/modules/menu.sh
     elif [ $DISTRO == "debian" ]
     then
         echo "Not yet supported."
@@ -30,7 +30,26 @@ distro_check(){
 
 }
 
-VERSION="dev branch"
+fedora_variant_check(){
+    test -f /run/ostree-booted && VARIANT=ostree
+    if [ ! -n "$VARIANT" ]
+    then
+        PKGMGR="dnf"
+        echo "Fedora spin not detected as immutable."
+        echo "Setting package manager to $PKGMGR."
+    elif [ $VARIANT == "ostree" ]
+    then
+        PKGMGR="rpm-ostree"
+        echo "Fedora spin detected as immutable"
+        echo "Setting package manager to $PKGMGR."
+        echo "Please note this is experimental atm"
+    fi
+}
+
+VERSION="menu-test branch"
+export DISTRO=""
+export PKGMGR=""
+export VARIANT=""
 export SCRIPTS_HOME=$(pwd)
 mkdir $SCRIPTS_HOME/temp        # make a temp folder for all files to be downloaded to
 launch_menu
