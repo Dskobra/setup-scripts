2012eea6e2f4192f5645d7a3ab7b734db541e365
Jordan Bottoms <jbttoms@gmail.com>
2024-02-03 03:19:08 -0600

    Update packages.sh
    added auto accept

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 install_third_party_repos(){
     if [ $PKGMGR == "dnf" ]
     then
-        sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+        sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     elif [ $PKGMGR == "rpm-ostree" ]
     then
         sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
