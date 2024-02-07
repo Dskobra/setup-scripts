0056fdd8b70ef2fb477f9c1f43a1204d78a9032b
Jordan Bottoms <jbttoms@gmail.com>
2024-02-07 07:37:07 -0600

    Update packages.sh
    added missing repo

--- packages.sh
+++ packages.sh
@@ -976,6 +976,7 @@ install_github_desktop(){
         curl -L -o shiftkey-gpg.key https://rpm.packages.shiftkey.dev/gpg.key
         sudo chown root:root shiftkey-gpg.key
         sudo mv shiftkey-gpg.key /etc/pki/rpm-gpg/
+        sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
         sudo rpm-ostree install git-gui github-desktop
         check_if_fedora_immutable
     elif [ $PKGMGR == "zypper" ]
