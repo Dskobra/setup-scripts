81068aa346278b68154466793a64b3da901b1c24
Jordan Bottoms <jbttoms@gmail.com>
2024-02-20 06:27:03 -0600

    Create raiderio.sh
    added launcher

---
+++ launchers/raiderio.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+## This launches an application with nohup
+## then separates the process. That way
+## i dont have to edit the shortcut with the
+## new appimage name everytime it's updated
+
+cd /home/$USER/.AppInstalls/
+# run raiderio while supressing input/output to avoid a logfile being made
+nohup ./RaiderIO_Client.appimage 0</dev/null 1>/dev/null 2>/dev/null &
