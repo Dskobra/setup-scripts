bbe92ef261201d71a38890e99912e06e845436bd
Jordan Bottoms <jbttoms@gmail.com>
2024-06-12 19:19:32 -0500

    Update dsksstuff.sh
    updated corectrl filename

--- dsksstuff.sh
+++ dsksstuff.sh
@@ -64,13 +64,13 @@ autostart(){
     DISCORD="/home/$USER/.local/share/flatpak/exports/share/applications/com.discordapp.Discord.desktop"
     DOVERLAY="/home/$USER/.local/share/flatpak/exports/share/applications/io.github.trigg.discover_overlay.desktop"
     STEAM="/usr/share/applications/steam.desktop"
-    CORECTRL="/usr/share/applications/org.corectrl.corectrl.desktop"
+    CORECTRL="/usr/share/applications/org.corectrl.CoreCtrl.desktop"
 
     [ -f $DROPBOX ] && { echo "Dropbox was found. Adding to startup."; cp "$DROPBOX"  /home/$USER/.config/autostart/com.dropbox.Client.desktop; }
     [ -f $DISCORD ] && { echo "Discord was found. Adding to startup."; cp "$DISCORD"  /home/$USER/.config/autostart/com.discordapp.Discord.desktop; }
     [ -f $DOVERLAY ] && { echo "Discord Overlay was found. Adding to startup."; cp "$DOVERLAY"  /home/$USER/.config/autostart/io.github.trigg.discover_overlay.desktop; }
     [ -f $STEAM ] && { echo "Steam was found. Adding to startup."; cp "$STEAM"  /home/$USER/.config/autostart/steam.desktop; }
-    [ -f $CORECTRL ] && { echo "Corectrl was found. Adding to startup."; cp "$CORECTRL"  /home/$USER/.config/autostart/org.corectrl.corectrl.desktop; }
+    [ -f $CORECTRL ] && { echo "Corectrl was found. Adding to startup."; cp "$CORECTRL"  /home/$USER/.config/autostart/org.corectrl.CoreCtrldesktop; }
 }
 
 install_spinfinity_theme(){
