1a45cc0293aeef4b248ba314655fd9feca532d2d
Jordan Bottoms <jbttoms@gmail.com>
2024-06-14 05:55:26 -0500

    Update packages.sh
    added auto confirm

--- packages.sh
+++ packages.sh
@@ -530,11 +530,11 @@ install_codecs(){
 install_amd_codecs(){
     if [ $PKGMGR == "dnf" ]
     then
-        sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
-        sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+        sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
+        sudo dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 
-        sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
-        sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+        sudo dnf swap -y mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+        sudo dnf swap -y mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
     elif [ $PKGMGR == "rpm-ostree" ]
     then
         echo "unfinished"
