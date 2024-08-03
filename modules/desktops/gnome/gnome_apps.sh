3be4d76268c1e39eb7e0294e12c40a807697482d
Jordan Bottoms <jbttoms@gmail.com>
2024-08-03 04:48:49 -0500

    Update gnome_apps.sh
    fixed missing install command

--- modules/desktops/gnome/gnome_apps.sh
+++ modules/desktops/gnome/gnome_apps.sh
@@ -49,7 +49,7 @@ packages_gnome(){
         gnome-extensions-app
     elif [ $PKGMGR == "rpm-ostree" ]
     then
-        sudo rpm-ostree file-roller evince dconf-editor pavucontrol cheese\
+        sudo rpm-ostree install file-roller evince dconf-editor pavucontrol cheese\
         gnome-extensions-app
         sudo rpm-ostree apply-live
         #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
