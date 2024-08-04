a4772d5684f491509e5f48e704d6fbc31ed8929a
Jordan Bottoms <jbttoms@gmail.com>
2024-08-03 21:34:54 -0500

    Update brave.sh
    Changed using dnf conf-manager on atomic editions due to gpg check issues. Now disables gpg check

--- modules/internet/brave.sh
+++ modules/internet/brave.sh
@@ -11,6 +11,7 @@ install_brave_browser(){
     read -r input
     if [ "$input" = 1 ]
     then
+        flatpak uninstall --user -y flathub com.brave.Browser
         package_brave_browser
     elif [ "$input" = 2 ] || [ -z "$input" ]
     then
@@ -34,10 +35,13 @@ package_brave_browser(){
         sudo dnf install -y brave-browser
     elif [ $PKGMGR == "rpm-ostree" ]
     then
-        sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
         curl -L -o brave-core.asc https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+        curl -L -o brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
         sudo chown root:root brave-core.asc
+        echo "gpgcheck=0" >> brave-browser.repo
+        sudo chown root:root brave-browser.repo
         sudo mv brave-core.asc /etc/pki/rpm-gpg/
+        sudo mv brave-browser.repo /etc/yum.repos.d
         sudo rpm-ostree refresh-md
         sudo rpm-ostree install brave-browser
         sudo rpm-ostree apply-live
@@ -79,4 +83,5 @@ remove_brave_browser(){
         echo "Unkown error has occurred."
     fi
 }
+
 install_brave_browser
