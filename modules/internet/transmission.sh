7de724604cf83f85e3e5081c2ca39c7a6ddd556e
Jordan Bottoms <jbttoms@gmail.com>
2024-08-05 23:33:20 -0500

    Update transmission.sh
    remove transmission when installing flatpak version

--- modules/internet/transmission.sh
+++ modules/internet/transmission.sh
@@ -14,7 +14,8 @@ install_transmission(){
         package_transmission
     elif [ "$input" = 2 ] || [ -z "$input" ]
     then
-       flatpak install --user -y  flathub com.transmissionbt.Transmission
+        remove_transmission
+        flatpak install --user -y  flathub com.transmissionbt.Transmission
     elif [ "$input" = 3 ]
     then
         $SCRIPTS_FOLDER/modules/core/packages_help_page.sh
@@ -40,4 +41,20 @@ package_transmission(){
     fi
 }
 
+remove_transmission(){
+    if [ $PKGMGR == "dnf" ]
+    then
+        sudo dnf remove -y transmission-gtk
+    elif [ $PKGMGR == "rpm-ostree" ]
+    then
+        sudo rpm-ostree remove transmission-gtk
+        sudo rpm-ostree apply-live
+        #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
+    elif [ $PKGMGR == "apt-get" ]
+    then
+        sudo apt-get remove -y transmission-gtk
+    else
+        echo "Unkown error has occurred."
+    fi
+}
 install_transmission
