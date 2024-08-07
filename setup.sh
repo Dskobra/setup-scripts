f1602c6647412ed7d09e137bd71af76fc321c2d6
Jordan Bottoms <jbttoms@gmail.com>
2024-08-07 02:34:23 -0500

    Update setup.sh
    Removed confirm_reboot function as it's been moved to modules/core

--- setup.sh
+++ setup.sh
@@ -102,31 +102,6 @@ fedora_variant_check(){
     fi
 }
 
-confirm_reboot(){
-    # Some packages cannot be applied live without restarting.
-    # Things like adding system groups, installing boot themes,
-    # steam udev rules (aka steam-devices)
-    # require rebooting first in order for them to be usable.
-    echo "================================================"
-    echo "Some packages won't be availble until a "
-    echo "restart is performed."
-    echo "Do you wish to restart now?"
-    echo "Type y/n"
-    echo "================================================"
-    printf "Option: "
-    read input
-    
-    if [ $input == "y" ] || [ $input == "Y" ]
-    then
-        sudo systemctl reboot
-    elif [ $input == "n" ] || [ $input == "N" ]
-    then
-        echo "Chose not to reboot."
-    else
-	    main_menu
-    fi
-}
-
 get_data(){
     # data branch includes links I can update more frequently and
     # my personal mangohud profiles (positioned for my liking).
