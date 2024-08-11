f9f8648ec0423dbe9361b4601dcf2db2385b6d0e
Jordan Bottoms <jbttoms@gmail.com>
2024-08-10 23:10:50 -0500

    Finished internet apps
    Cleaned up apps

---
+++ modules/flatpak/internet/brave.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+remove_brave_browser(){
+    if [ $PKGMGR == "dnf" ]
+    then
+        sudo dnf remove -y brave-browser
+        sudo rm /etc/yum.repos.d/brave-browser.repo
+        sudo rm /etc/pki/rpm-gpg/brave-core.asc
+        sudo dnf update -y
+        
+    elif [ $PKGMGR == "rpm-ostree" ]
+    then
+        sudo rpm-ostree uninstall brave-browser
+        sudo rm /etc/yum.repos.d/brave-browser.repo
+        sudo rm /etc/pki/rpm-gpg/brave-core.asc
+        sudo rpm-ostree refresh-md
+        sudo rpm-ostree apply-live
+        #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
+    elif [ $PKGMGR == "apt-get" ]
+    then
+        sudo apt-get remove -y brave-browser
+        sudo rm /etc/apt/sources.list.d/brave-browser-release.list
+        sudo apt-get update
+        
+    else
+        echo "Unkown error has occurred."
+    fi
+}
+
+flatpak install --user -y flathub com.brave.Browser
+remove_brave_browser
---
+++ modules/flatpak/internet/transmission.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+remove_transmission(){
+    if [ $PKGMGR == "dnf" ]
+    then
+        sudo dnf remove -y transmission-gtk
+    elif [ $PKGMGR == "rpm-ostree" ]
+    then
+        sudo rpm-ostree remove transmission-gtk
+        sudo rpm-ostree apply-live
+        #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
+    elif [ $PKGMGR == "apt-get" ]
+    then
+        sudo apt-get remove -y transmission-gtk
+    else
+        echo "Unkown error has occurred."
+    fi
+}
+
+flatpak install --user -y  flathub com.transmissionbt.Transmission
+remove_transmission
