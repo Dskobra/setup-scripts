08deb3ab92238cd3837f4e3d55b28c7c57c58ff0
Jordan Bottoms <jbttoms@gmail.com>
2024-08-11 01:36:46 -0500

    Finished utilities

--- modules/native/utilities/gtkhash.sh
+++ modules/native/utilities/gtkhash.sh
@@ -1,31 +1,6 @@
 
 #!/usr/bin/bash
 
-install_gtkhash(){
-    echo "-------Pick an option-------"
-    echo "(1) distro built app"
-    echo "(2) distro neutral flatpak"
-    echo "(3) for help"
-    echo "(empty) default option which is flatpak"
-    echo "----------------------------"
-    printf "Option: "
-    read -r input
-    if [ "$input" = 1 ]
-    then
-        flatpak remove --user -y org.gtkhash.gtkhash
-        package_gtkhash
-    elif [ "$input" = 2 ] || [ -z "$input" ]
-    then
-       flatpak install --user -y flathub org.gtkhash.gtkhash
-       remove_gtkhash
-    elif [ "$input" = 3 ]
-    then
-        $SCRIPTS_FOLDER/modules/core/packages_help_page.sh
-    else
-        echo "Invalid option"
-    fi
-}
-
 package_gtkhash(){
     if [ $PKGMGR == "dnf" ]
     then
@@ -43,21 +18,5 @@ package_gtkhash(){
     fi
 }
 
-remove_gtkhash(){
-    if [ $PKGMGR == "dnf" ]
-    then
-        sudo dnf remove -y gtkhash
-    elif [ $PKGMGR == "rpm-ostree" ]
-    then
-        sudo rpm-ostree uninstall gtkhash
-        sudo rpm-ostree apply-live
-        #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
-    elif [ $PKGMGR == "apt-get" ]
-    then
-        sudo apt-get remove -y gtkhash
-    else
-        echo "Unkown error has occurred."
-    fi
-}
-
-install_gtkhash
+flatpak remove --user -y org.gtkhash.gtkhash
+package_gtkhash
--- modules/native/utilities/rpi_imager.sh
+++ modules/native/utilities/rpi_imager.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/bash
 
-install_rpi_imager(){
-    echo "-------Pick an option-------"
-    echo "(1) distro built app"
-    echo "(2) distro neutral flatpak"
-    echo "(3) for help"
-    echo "(empty) default option which is flatpak"
-    echo "----------------------------"
-    printf "Option: "
-    read -r input
-    if [ "$input" = 1 ]
-    then
-        flatpak remove --user -y org.raspberrypi.rpi-imager
-        package_rpi_imager
-    elif [ "$input" = 2 ] || [ -z "$input" ]
-    then
-       flatpak install --user -y flathub org.raspberrypi.rpi-imager
-       remove_rpi_imager
-    elif [ "$input" = 3 ]
-    then
-        $SCRIPTS_FOLDER/modules/core/packages_help_page.sh
-    else
-        echo "Invalid option"
-    fi
-}
-
 package_rpi_imager(){
     ## template function for adding more packages
     if [ $PKGMGR == "dnf" ]
@@ -38,28 +13,10 @@ package_rpi_imager(){
     elif [ $PKGMGR == "apt-get" ]
     then
         zenity --info --text="Raspberry Pi Imager isn't currently available in Debian. This will install the flatpak version."
-        flatpak install --user -y flathub org.raspberrypi.rpi-imager
+        $SCRIPTS_FOLDER/modules/flatpak/utilities/rpi_imager.sh
     else
         echo "Unkown error has occurred."
     fi
 }
 
-remove_rpi_imager(){
-    ## template function for adding more packages
-    if [ $PKGMGR == "dnf" ]
-    then
-        sudo dnf remove -y rpi-imager
-    elif [ $PKGMGR == "rpm-ostree" ]
-    then
-        sudo rpm-ostree uninstall rpi-imager
-        sudo rpm-ostree apply-live
-        #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
-    elif [ $PKGMGR == "apt-get" ]
-    then
-        echo "Not removing Raspberry Pi Imager as it's not present in Debian repos."
-    else
-        echo "Unkown error has occurred."
-    fi
-}
-
-install_rpi_imager
+package_rpi_imager
