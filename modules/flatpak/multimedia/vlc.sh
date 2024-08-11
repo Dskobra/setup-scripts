489a6032c0d7bb18812d18c377233a8779f5dc9e
Jordan Bottoms <jbttoms@gmail.com>
2024-08-10 23:04:09 -0500

    Cleaned up multimedia apps

---
+++ modules/flatpak/multimedia/vlc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+remove_vlc(){
+    if [ $PKGMGR == "dnf" ]
+    then
+        sudo dnf remove -y vlc
+    elif [ $PKGMGR == "rpm-ostree" ]
+    then
+        sudo rpm-ostree uninstall vlc
+        sudo rpm-ostree apply-live
+        #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
+    elif [ $PKGMGR == "apt-get" ]
+    then
+        sudo apt-get remove -y vlc
+    else
+        echo "Unkown error has occurred."
+    fi
+}
+
+flatpak install --user -y flathub org.videolan.VLC
+remove_vlc
