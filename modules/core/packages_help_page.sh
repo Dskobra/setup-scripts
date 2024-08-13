f186e17c8dc611202b933595489cc9e7a0c5f6b5
Jordan Bottoms <jbttoms@gmail.com>
2024-08-13 00:42:52 -0500

    Update packages_help_page.sh

--- modules/core/packages_help_page.sh
+++ modules/core/packages_help_page.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/bash
 
-packages_help_page(){
-    xdg-open https://github.com/Dskobra/setup-scripts/wiki/Package-install-methods
-}
-
-packages_help_page
+xdg-open https://github.com/Dskobra/setup-scripts/wiki/Package-install-methods
