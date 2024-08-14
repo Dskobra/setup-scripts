3acfce23f36135e7e020e5d6503970968a96e7af
Jordan Bottoms <jbttoms@gmail.com>
2024-08-14 01:48:05 -0500

    Update setup.sh
    Fixing code warnings :P

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@
 ### determination and making app/temp folders.
 
 make_temp(){
-    test -d $SCRIPTS_FOLDER/temp && TEMP_FOLDER=exists
+    test -d $SCRIPTS_FOLDER/temp && TEMP_FOLDER="exists"
     if [ "$TEMP_FOLDER" = "exists" ];
         then
-           TEMP_FOLDER=exists 
+           TEMP_FOLDER="exists"
     elif [ "$TEMP_FOLDER" = "missing" ];
         then
         mkdir $SCRIPTS_FOLDER/temp        # make a temp folder for all files to be downloaded to   
@@ -17,17 +17,17 @@ make_temp(){
 make_app_folder(){
     ### Store netbeans, intellij idea and pycharm
     ### in ~/Apps  
-    test -d $HOME/Apps && LOOK_FOR_APP_FOLDER=exists
+    test -d $HOME/Apps && LOOK_FOR_APP_FOLDER="exists"
     if [ "$LOOK_FOR_APP_FOLDER" = "exists" ];
         then
-           LOOK_FOR_APP_FOLDER=exists 
+           LOOK_FOR_APP_FOLDER="exists"
     elif [ "$LOOK_FOR_APP_FOLDER" = "missing" ];
         then
             mkdir $APP_FOLDER
     fi
 }
 
-export SCRIPTS_FOLDER=$(pwd)        # stores full path for setup-scripts
+export SCRIPTS_FOLDER       # stores full path for setup-scripts
 export APP_FOLDER="$HOME/Apps"      # app folder thats made for some downloads
 export PKGMGR=""                    # stores package manager name such as dnf/rpm-ostree etc
 export COPYRIGHT="Copyright (c) 2021-2024 Jordan Bottoms"
@@ -35,6 +35,7 @@ export VERSION="7.21.2024"
 export PACKAGE_TYPE="flatpak"
 TEMP_FOLDER="missing"
 LOOK_FOR_APP_FOLDER="missing"
+SCRIPTS_FOLDER=$(pwd)
 make_temp
 make_app_folder
 $SCRIPTS_FOLDER/modules/core/distro_check.sh
