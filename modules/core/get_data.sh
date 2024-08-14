94960ffce9bd80fad66de2f524e7245ded8620c9
Jordan Bottoms <jbttoms@gmail.com>
2024-08-14 02:28:54 -0500

    Update get_data.sh

--- modules/core/get_data.sh
+++ modules/core/get_data.sh
@@ -4,10 +4,10 @@ get_data(){
     # data branch includes links I can update more frequently and
     # my personal mangohud profiles (positioned for my liking).
     echo "Will need to download extra files from data branch"
-    cd $SCRIPTS_FOLDER
+    cd "$SCRIPTS_FOLDER" || exit
     rm -r -f data
     git clone https://github.com/Dskobra/setup-scripts -b data
-    mv $SCRIPTS_FOLDER/setup-scripts $SCRIPTS_FOLDER/data
+    mv "$SCRIPTS_FOLDER"/setup-scripts "$SCRIPTS_FOLDER"/data
 }
 
 get_data
