b6b3815e00a17f074c56483b357040dbc43a0100
Jordan Bottoms <jbttoms@gmail.com>
2024-08-15 06:07:27 -0500

    Update kate.sh
    fixed typo

--- modules/native/desktops/kde/kate.sh
+++ modules/native/desktops/kde/kate.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/bash
 
 install_kate(){
-    ## template function for adding more packages
-    if [ "$PKGMG"R == "dnf" ]
+    if [ "$PKGMGR" == "dnf" ]
     then
         sudo dnf install -y kate kate-plugins
-    elif [ "$PKGMG"R == "rpm-ostree" ]
+    elif [ "$PKGMGR" == "rpm-ostree" ]
     then
         sudo rpm-ostree install kate kate-plugins --allow-inactive
         sudo rpm-ostree apply-live     
         #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
-    elif [ "$PKGMG"R == "apt-get" ]
+    elif [ "$PKGMGR" == "apt-get" ]
     then
         sudo apt-get install -y kate
     else
