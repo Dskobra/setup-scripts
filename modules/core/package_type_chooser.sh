193b30d068a7b9723c93b4a8e16184eb1890bbf6
Jordan Bottoms <jbttoms@gmail.com>
2024-08-16 18:45:11 -0500

    Update package_type_chooser.sh
    added exit option for package type chooser

--- modules/core/package_type_chooser.sh
+++ modules/core/package_type_chooser.sh
@@ -7,9 +7,9 @@
 
 package_type_chooser(){
     echo "Please input the package type you'd like to use."
-    echo "--------------------------------"
-    echo "[1] Flatpak [2] Native [3] Help"
-    echo "---------------------------------"
+    echo "----------------------------------------"
+    echo "[1] Flatpak [2] Native [3] Help [0] Exit"
+    echo "----------------------------------------"
     echo "(empty) default option which is flatpak"
     printf "Option: "
     read -r input
@@ -24,8 +24,14 @@ package_type_chooser(){
     elif [ "$input" = 3 ]
     then
         "$SCRIPTS_FOLDER"/modules/core/packages_help_page.sh
+        package_type_chooser
+    elif [ "$input" = 0 ]
+    then
+        echo "Will now exit."
+        exit
     else
-        echo "Unkown error has occurred."
+        echo "Invalid option or error has occurred."
     fi
 }
+
 package_type_chooser
