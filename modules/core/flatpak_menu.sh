6beabea79aac2e398c73a6e471e7c0bf80ac3e16
Jordan Bottoms <jbttoms@gmail.com>
2024-08-16 03:03:46 -0500

    Update flatpak_menu.sh
    Installed podman desktop before installing container packages on atomic edition

--- modules/core/flatpak_menu.sh
+++ modules/core/flatpak_menu.sh
@@ -1042,8 +1042,8 @@ dev_other_menu(){
             ;;
 
         2)
-            "$SCRIPTS_FOLDER"/modules/native/development/containers.sh
             "$SCRIPTS_FOLDER"/modules/flatpak/development/podman_desktop.sh
+            "$SCRIPTS_FOLDER"/modules/native/development/containers.sh
             ;;
 
         p)
