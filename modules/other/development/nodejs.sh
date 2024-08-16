53410f859a94ade684d6debad539cd9e088c9fd1
Jordan Bottoms <jbttoms@gmail.com>
2024-08-16 06:38:23 -0500

    Update nodejs.sh
    Now just inform user with zenity to manually restart shell and run nvm install lts/*

--- modules/other/development/nodejs.sh
+++ modules/other/development/nodejs.sh
@@ -5,16 +5,23 @@
 #LTS release.
 
 install_nodejs(){
-    test -d "$HOME"/.nvm && NVM_FOLDER="exists"
-    if [ "$NVM_FOLDER" = "exists" ];
-        then
-           nvm install lts/*
-    elif [ "$NVM_FOLDER" = "missing" ];
-        then
-			curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-			zenity --info --text="Please rerun this option after restarting your shell otherwise it won't see nvm"
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+    if [ "$PKGMGR" == "dnf" ] || [ "$PKGMGR" == "rpm-ostree" ]
+    then
+		source ~/.bashrc
+        nvm install lts/*
+    elif [ "$PKGMGR" == "apt-get" ]
+    then
+        # Due to some unknown reason/issue on Debian nvm is unable to be run from
+        # this script. source ~/.bashrc won't work nor will rerunning this after a shell
+        # restart. Manually is the only way.
 
+        zenity --info --text="Please open a new shell and run\nnvm install lts/*"
+    else
+        echo "Unkown error has occurred."
     fi
 }
-NVM_FOLDER="missing"
+
 install_nodejs
+
+# random note, but on debian it puts nvm into ~/.nvm while fedora is ~/.config/nvm
