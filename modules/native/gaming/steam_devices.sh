ec5ad872f7ca3a130a60f903083bd84317d61d11
Jordan Bottoms <jbttoms@gmail.com>
2024-08-16 02:12:39 -0500

    Update steam_devices.sh
    Unintentionally removed steam-device support from atomic editions

--- modules/native/gaming/steam_devices.sh
+++ modules/native/gaming/steam_devices.sh
@@ -4,6 +4,11 @@ install_steam_devices(){
     if [ "$PKGMGR" == "dnf" ]
     then
         sudo dnf install -y steam-devices
+    elif [ "$PKGMGR" == "rpm-ostree" ]
+    then
+        sudo rpm-ostree install steam-devices
+        #sudo rpm-ostree apply-live
+        "$SCRIPTS_FOLDER"/modules/core/confirm_reboot.sh
     elif [ "$PKGMGR" == "apt-get" ]
     then
         sudo dpkg --add-architecture i386
