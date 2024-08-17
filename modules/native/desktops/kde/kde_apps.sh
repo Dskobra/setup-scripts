1e5bcb17e72fa055bc1164f9b743f71a427cc126
Jordan Bottoms <jbttoms@gmail.com>
2024-08-16 19:02:35 -0500

    Update kde_apps.sh
    Fixed typo

--- modules/native/desktops/kde/kde_apps.sh
+++ modules/native/desktops/kde/kde_apps.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 install_kde_apps(){
-    if [ "$PKGMG"R == "dnf" ]
+    if [ "$PKGMGR" == "dnf" ]
     then
         sudo dnf install -y ark kate krdc kcalc kamoso gwenview\
         kleopatra okular
-    elif [ "$PKGMG"R == "apt-get" ]
+    elif [ "$PKGMGR" == "apt-get" ]
     then
         sudo apt-get install -y ark kate krdc kcalc kamoso\
         gwenview okular kleopatra
