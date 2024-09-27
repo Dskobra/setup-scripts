87e0be17d4a1655dd41980f851ac7176bb850088
Jordan Bottoms <jbttoms@gmail.com>
2024-09-27 04:21:01 -0500

    added a menu
    Also adjusted the window size and added bootstrap

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,15 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
-        width: 1000,
+        width: 900,
         height: 400,
         resizable: false
     })
     
     mainWindow.loadFile('index.html')
-    mainWindow.setMenu(null)
+    //mainWindow.setMenu(null)          // menu enabled for debugging. Might create a custom toolbar later
 }
 
 app.whenReady().then(() => {
