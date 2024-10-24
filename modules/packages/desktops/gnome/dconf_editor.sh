7c3dff0a8b751830577a4d2cf29ff4875e9b4019
Jordan Bottoms <jbttoms@gmail.com>
2024-10-23 21:23:32 -0500

    Moved files
    Moved files into new folder

---
+++ modules/packages/desktops/gnome/dconf_editor.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/bash
+
+native_dconf_editor(){
+    if [ "$PKGMGR" == "dnf" ]
+    then
+        sudo dnf install -y dconf-editor
+    elif [ "$PKGMGR" == "apt-get" ]
+    then
+        sudo apt-get install -y dconf-editor
+    else
+        echo "Unkown error has occurred."
+    fi
+    flatpak remove --user -y ca.desrt.dconf-editor
+}
+
+remove_dconf_editor(){
+    if [ "$PKGMGR" == "dnf" ]
+    then
+        sudo dnf remove -y dconf-editor
+    elif [ "$PKGMGR" == "rpm-ostree" ]
+    then
+        echo "Not removing package on atomic editions."
+    elif [ "$PKGMGR" == "apt-get" ]
+    then
+        sudo apt-get remove -y dconf-editor
+    else
+        echo "Unkown error has occurred."
+    fi
+}
+
+
+if [ "$1" == "flatpak" ]
+then
+    flatpak install --user -y flathub ca.desrt.dconf-editor
+    remove_dconf_editor
+elif [ "$1" == "distro" ]
+then
+    native_dconf_editor
+else
+    echo "error"
+fi
