7d2fd0cd671a35a9f9aa30e345f756c7027ef87f
Jordan Bottoms <jbttoms@gmail.com>
2024-10-29 19:22:39 -0500

    Update gnome_tweaks.sh
    Request reboot instead of attempting to apply layer

--- modules/packages/desktops/gnome/gnome_tweaks.sh
+++ modules/packages/desktops/gnome/gnome_tweaks.sh
@@ -7,8 +7,8 @@ native_gnome_tweaks(){
     elif [ "$PKGMGR" == "rpm-ostree" ]
     then
         sudo rpm-ostree install gnome-tweaks
-        sudo rpm-ostree apply-live
-        #$SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
+        #sudo rpm-ostree apply-live
+        $SCRIPTS_FOLDER/modules/core/confirm_reboot.sh
     elif [ "$PKGMGR" == "apt-get" ]
     then
         sudo apt-get install -y gnome-tweaks
