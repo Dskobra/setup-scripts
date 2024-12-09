4211e4f5c208c33b2ba35a143d075e96777ae9ed
Jordan Bottoms <jbttoms@gmail.com>
2024-12-09 06:07:28 -0600

    Updated vscodium commands
    updated vscodium install commands

--- modules/packages/development/vscodium.sh
+++ modules/packages/development/vscodium.sh
@@ -3,24 +3,22 @@
 native_vscodium(){
     if [ "$PKGMGR" == "dnf" ]
     then
-        cd "$SCRIPTS_FOLDER"/modules/packages/development
-        cp vscodium.repo.txt vscodium.repo
-        sudo chown root:root vscodium.repo
-        sudo mv vscodium.repo /etc/yum.repos.d/vscodium.repo
+        sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+        printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
         sudo dnf install -y codium
     elif [ "$PKGMGR" == "zypper" ]
     then
-        cd "$SCRIPTS_FOLDER"/modules/packages/development
-        cp vscodium.repo.txt vscodium.repo
-        sudo chown root:root vscodium.repo
-        sudo mv vscodium.repo /etc/zypp/repos.d/vscodium.repo
+        sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+        printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/zypp/repos.d/vscodium.repo
         sudo zypper -n install codium
     elif [ "$PKGMGR" == "apt-get" ]
     then
-        wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
-        sudo apt-add-repository 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main'
+        wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+            | gpg --dearmor \
+            | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+        echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+            | sudo tee /etc/apt/sources.list.d/vscodium.list
         sudo apt-get update && sudo apt-get install -y codium
-        
     else
         echo "Unkown error has occurred."
     fi
