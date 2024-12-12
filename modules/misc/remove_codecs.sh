d9c3111c41831ee06ab097dfbca21e59a1f4c31d
Jordan Bottoms <jbttoms@gmail.com>
2024-12-12 05:17:47 -0600

    Merged codec removal scripts

--- modules/misc/remove_codecs.sh
+++ modules/misc/remove_codecs.sh
@@ -3,20 +3,28 @@
 ### This is for swapping the ffmpeg codecs from rpmfusion
 ### back to the limited ones from Fedora repos.
 remove_ffmpeg(){
-    if [ "$PKGMGR" == "dnf" ]
+    if [ "$DISTRO" == "fedora" ]
     then
         sudo dnf swap -y ffmpeg ffmpeg-free --allowerasing
-    elif [ "$PKGMGR" == "rpm-ostree" ]
+
+        sudo dnf remove -y mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld
+        sudo dnf remove -y mesa-va-drivers-freeworld.i686 mesa-vdpau-drivers-freeworld.i686
+    elif [ "$DISTRO" == "fedora-atomic" ]
     then
         sudo rpm-ostree uninstall ffmpeg
         sudo rpm-ostree override reset ffmpeg-free libavcodec-free libavdevice-free libavfilter-free \
         libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free
+
+        sudo rpm-ostree uninstall mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld
+        sudo rpm-ostree uninstall mesa-va-drivers-freeworld.i686 mesa-vdpau-drivers-freeworld.i686
+
+        sudo rpm-ostree override reset mesa-va-drivers
         "$SCRIPTS_FOLDER"/modules/core/confirm_reboot.sh
-    elif [ "$PKGMGR" == "apt-get" ]
+    elif [ "$DISTRO" == "debian" ]
     then
         echo "================================================================"
-        echo "This is only for Fedora as it swaps the RPMFusion ffmpeg"
-        echo "for the Fedora provided ones which only uses patent free codecs."
+        echo "This is only for Fedora as it swaps the RPMFusion media codecs"
+        echo "for the Fedora provided ones which are patent free."
         echo "================================================================"
     else
         echo "Unkown error has occurred."
