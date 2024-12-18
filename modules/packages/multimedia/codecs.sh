6bd49953fe180ebe959ef290c85ae313c1780660
Jordan Bottoms <jbttoms@gmail.com>
2024-12-18 06:36:34 -0600

    Update codecs.sh
    added missing auto accept

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -17,7 +17,7 @@ native_codecs(){
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
         sudo zypper ref
         sudo zypper dist-upgrade --from packman-essentials --allow-vendor-change
-        sudo zypper install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
+        sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
 
     elif [ "$DISTRO" == "debian" ]
     then
