dd50635f80ab0c1fb5256e893a4570752e687d86
Jordan Bottoms <jbttoms@gmail.com>
2024-12-21 23:34:57 -0600

    Update check_for_libvirt_group.sh
    Updated error string

--- modules/misc/check_for_libvirt_group.sh
+++ modules/misc/check_for_libvirt_group.sh
@@ -6,8 +6,8 @@ check_for_libvirt_group(){
     if [ $(getent group libvirt) ]; then
         sudo usermod -aG libvirt "$USER"
     else
-        echo "Group doesn't exist. You're likely running Fedora Atomic Edition."
-        echo "Go to Misc menu and choose 'Add user to libvirt group' after rebooting."
+        echo "Group doesn't exist. Please select the 'Virtualization' option"
+        echo "under 'Utilities' menu with 'Native' apps."
     fi
 }
 
