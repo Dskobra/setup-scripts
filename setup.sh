2922ea9272c63f38a5f759df4e9e1b7d441ed6b3
Jordan Bottoms <jbttoms@gmail.com>
2024-12-22 00:32:59 -0600

    Update setup.sh
    version bump

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ export SCRIPTS_FOLDER                           # stores full path for setup-scr
 export APP_FOLDER="$HOME/Apps"                  # app folder thats made for some downloads
 export DISTRO=""                                # stores distro name.
 export COPYRIGHT="Copyright (c) 2021-2024 Jordan Bottoms"
-export VERSION="11.30.2024"
+export VERSION="12.22.2024"
 export PACKAGE_TYPE="flatpak"
 TEMP_FOLDER="missing"
 LOOK_FOR_APP_FOLDER="missing"
