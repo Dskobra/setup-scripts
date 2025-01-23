a7f26ffcfcb51592b6c16e34e8c9394be9f6c7a6
Jordan Bottoms <jbttoms@gmail.com>
2025-01-22 22:12:23 -0600

    Update setup.sh
    Removed old apps folder

--- setup.sh
+++ setup.sh
@@ -9,8 +9,7 @@ export DISTRO=""                                # stores distro name.
 export COPYRIGHT="Copyright (c) 2021-2025 Jordan Bottoms"
 export VERSION="1.5.2025"
 SCRIPTS_FOLDER=$(pwd)
-mkdir $SCRIPTS_FOLDER/temp
-mkdir /home/$USER/Apps/                         # old location. Remove once everything is updated
+mkdir $SCRIPTS_FOLDER/temp                       
 mkdir /home/$USER/bin
 sudo mkdir /opt/apps/
 sudo mkdir /opt/apps/icons
