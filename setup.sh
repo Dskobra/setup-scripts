9b773259b0210933d81be38bcf1865b2077a18d5
Jordan Bottoms <jbttoms@gmail.com>
2025-01-30 20:56:05 -0600

    Update setup.sh
    Removed no longer used variable

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 ### and sets some variables
 
 export SCRIPTS_FOLDER                           # stores full path for setup-scripts
-export APP_FOLDER="$HOME/Apps"                  # app folder thats made for some downloads
 export DISTRO=""                                # stores distro name.
 export COPYRIGHT="Copyright (c) 2021-2025 Jordan Bottoms"
 export VERSION="1.5.2025"
