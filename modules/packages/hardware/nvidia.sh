877c24dd5d3a076e252396ffd2a28ef6b66fa91d
Jordan Bottoms <jbttoms@gmail.com>
2025-02-03 03:30:22 -0600

    Updated nvidia instructions
    Moved instructions for opensuse to a text file

--- modules/packages/hardware/nvidia.sh
+++ modules/packages/hardware/nvidia.sh
@@ -9,17 +9,8 @@ install_nvidia(){
         xdg-open https://rpmfusion.org/Howto/NVIDIA?highlight=%28%5CbCategoryHowto%5Cb%29#Installing_the_drivers
     elif [ "$DISTRO" == "opensuse-tumbleweed" ] || [ "$DISTRO" == "opensuse-slowroll" ]
     then
-        echo "#####################nvidia GPU Instructions#####################"
-        echo "1. Please open the KDE menu and search for YaST and enter your password when requested."
-        echo "2. Open Software Management and there should be a list of packages to install on the right"
-        echo "if it is a fresh install of openSUSE."
-        echo "3. Verify either openSUSE-repos-Slowroll-NVIDIA or openSUSE-repos-Tumbleweed-NVIDIA "
-        echo "is selected for install."
-        echo "4. Click Accept to install and Finish to close out."
-        echo "5. With YaST still open click Software Repositories near the top."
-        echo "6. Click refresh near the bottom right and select Refresh Enabled and accept the GnuPG Key."
-        echo "7. Reopen Software Management and it should have auto selected the drivers for install on the right."
-        echo "8. Click Accept and accept the licenses. Once done click Finish and reboot!"
+
+        xdg-open "$SCRIPTS_FOLDER"/modules/packages/hardware/nvidia.txt
         xdg-open "https://en.opensuse.org/SDB:NVIDIA_drivers#Via_YaST_(for_Leap_and_Tumbleweed)"
     else
         echo "Unkown error has occurred."
