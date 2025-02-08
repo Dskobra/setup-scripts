52bea3ded5e9b69ca3f33f87bdba09c12b4c0e12
Jordan Bottoms <jbttoms@gmail.com>
2025-02-07 22:09:25 -0600

    Update codecs.sh
    Reordered vendor change command

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -16,15 +16,14 @@ native_codecs(){
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
         sudo zypper ref
-        sudo zypper dup --from packman-essentials --allow-vendor-change
         sudo zypper install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
-
+        sudo zypper dup --from packman-essentials --allow-vendor-change
     elif [ "$DISTRO" == "opensuse-slowroll" ]
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/' packman-essentials
         sudo zypper ref
-        sudo zypper dup --from packman-essentials --allow-vendor-change
         sudo zypper  install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
+        sudo zypper dup --from packman-essentials --allow-vendor-change
     else
         echo "Unkown error has occurred."
     fi
