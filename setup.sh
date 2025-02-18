8de88e386ed6e817e55525defc169a4bb816dad5
Jordan Bottoms <jbttoms@gmail.com>
2025-02-17 23:48:21 -0600

    Redid prereq code
    Set prereq.txt to 1 when core packages are installed and moved creating apps folder into first run

--- setup.sh
+++ setup.sh
@@ -9,9 +9,4 @@ export COPYRIGHT="Copyright (c) 2021-2025 Jordan Bottoms"
 export VERSION="2.6.2025"
 SCRIPTS_FOLDER=$(pwd)
 mkdir $SCRIPTS_FOLDER/temp                      
-mkdir /home/$USER/bin
-sudo mkdir /opt/apps/
-sudo mkdir /opt/apps/icons
-sudo mkdir /opt/apps/appimages
-sudo chown $USER:$USER /opt/apps/ -R
 "$SCRIPTS_FOLDER"/modules/core/launcher.sh
