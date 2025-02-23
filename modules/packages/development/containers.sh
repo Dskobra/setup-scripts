4355df8a91f26cb0df517129099870c3af67f307
Jordan Bottoms <jbttoms@gmail.com>
2025-02-23 00:36:23 -0600

    Update containers.sh
    install docker-compose-switch which is needed for podman desktop compose extension

--- modules/packages/development/containers.sh
+++ modules/packages/development/containers.sh
@@ -3,10 +3,11 @@
 native_containers(){
     if [ "$DISTRO" == "fedora" ]
     then
-        sudo dnf install -y toolbox distrobox
+        sudo dnf install -y toolbox distrobox docker-compose-switch
     elif [ "$DISTRO" == "opensuse-tumbleweed" ] || [ "$DISTRO" == "opensuse-slowroll" ]
     then
         sudo zypper -n install patterns-containers-container_runtime toolbox distrobox
+        sudo zypper -n install docker-compose-switch
     else
         echo "Unkown error has occurred."
     fi
