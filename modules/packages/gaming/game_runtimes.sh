beef09634269817a63855b9dc15bbd38ffce54a3
Jordan Bottoms <jbttoms@gmail.com>
2025-02-23 21:59:32 -0600

    Update game_runtimes.sh
    Reordered plus/tricks flatpaks so they install regardless if native/flatpak was selected

--- modules/packages/gaming/game_runtimes.sh
+++ modules/packages/gaming/game_runtimes.sh
@@ -12,14 +12,13 @@ native_game_runtimes(){
     fi
 }
 
-mkdir $HOME/.config/MangoHud           
+mkdir $HOME/.config/MangoHud
+flatpak install --user -y flathub com.vysp3r.ProtonPlus
+flatpak install --user -y flathub com.github.Matoking.protontricks           
 if [ "$1" == "flatpak" ]
 then
     flatpak install --user -y runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08
     flatpak install --user -y flathub org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08
-
-    flatpak install --user -y flathub com.vysp3r.ProtonPlus
-    flatpak install --user -y flathub com.github.Matoking.protontricks
 elif [ "$1" == "native" ]
 then
     native_game_runtimes
