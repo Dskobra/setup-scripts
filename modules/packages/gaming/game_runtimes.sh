2ab41ffb2ee92eba3f0df8d774aa0302637a4207
Jordan Bottoms <jbttoms@gmail.com>
2025-02-23 21:56:34 -0600

    Update game_runtimes.sh
    Added protonplus/tricks into game runtimes script and removed gamescope

--- modules/packages/gaming/game_runtimes.sh
+++ modules/packages/gaming/game_runtimes.sh
@@ -3,10 +3,10 @@
 native_game_runtimes(){
     if [ "$DISTRO" == "fedora" ]
     then
-        sudo dnf install -y mangohud gamescope gamemode
+        sudo dnf install -y mangohud gamemode
     elif [ "$DISTRO" == "opensuse-tumbleweed" ] || [ "$DISTRO" == "opensuse-slowroll" ]
     then
-        sudo zypper -n install mangohud gamescope gamemode
+        sudo zypper -n install mangohud gamemode
     else
         echo "Unkown error has occurred."
     fi
@@ -17,6 +17,9 @@ if [ "$1" == "flatpak" ]
 then
     flatpak install --user -y runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08
     flatpak install --user -y flathub org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08
+
+    flatpak install --user -y flathub com.vysp3r.ProtonPlus
+    flatpak install --user -y flathub com.github.Matoking.protontricks
 elif [ "$1" == "native" ]
 then
     native_game_runtimes
