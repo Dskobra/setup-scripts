6f04e75bbdb106ed3740d4bd87f9aa15523053a8
Jordan Bottoms <jbttoms@gmail.com>
2025-02-25 02:44:24 -0600

    Update game_runtimes.sh
    Removed gamescope flatpak

--- modules/packages/gaming/game_runtimes.sh
+++ modules/packages/gaming/game_runtimes.sh
@@ -18,7 +18,6 @@ flatpak install --user -y flathub com.github.Matoking.protontricks
 if [ "$1" == "flatpak" ]
 then
     flatpak install --user -y runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08
-    flatpak install --user -y flathub org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08
 elif [ "$1" == "native" ]
 then
     native_game_runtimes
