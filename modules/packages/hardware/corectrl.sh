a0e7625e22981157e8ac314efd6c49c41c1269ba
Jordan Bottoms <jbttoms@gmail.com>
2025-03-08 23:48:10 -0600

    Update corectrl.sh
    changed back to dead mozay repo for corectrl

--- modules/packages/hardware/corectrl.sh
+++ modules/packages/hardware/corectrl.sh
@@ -6,12 +6,12 @@ install_corectrl(){
         xdg-open https://gitlab.com/corectrl/corectrl/-/wikis/Setup
     elif [ "$DISTRO" == "opensuse-tumbleweed" ]
     then
-        sudo zypper addrepo https://download.opensuse.org/repositories/home:ecsos/openSUSE_Tumbleweed/home:ecsos.repo
+        sudo zypper addrepo https://download.opensuse.org/repositories/home:Dead_Mozay/openSUSE_Tumbleweed/home:Dead_Mozay.repo
         sudo zypper ref
         sudo zypper -n install corectrl
     elif [ "$DISTRO" == "opensuse-slowroll" ]
     then
-        sudo zypper addrepo https://download.opensuse.org/repositories/home:ecsos/openSUSE_Slowroll/home:ecsos.repo
+        sudo zypper addrepo https://download.opensuse.org/repositories/home:Dead_Mozay/openSUSE_Slowroll/home:Dead_Mozay.repo
         sudo zypper refresh
         sudo zypper -n install corectrl
     else
