9827ee4d3fef9cd22708739aef609d39131c8ee5
Jordan Bottoms <jbttoms@gmail.com>
2025-03-10 03:11:04 -0500

    Update nvidia.sh
    cleanup

--- modules/packages/hardware/nvidia.sh
+++ modules/packages/hardware/nvidia.sh
@@ -4,7 +4,7 @@ install_nvidia(){
     if [ "$DISTRO" == "fedora" ]
     then
         #sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda nvidia-settings
-        # Use open kernel modules
+        #Use open kernel modules
         sudo dnf install -y rpmfusion-nonfree-release-tainted
         sudo dnf install -y akmod-nvidia-open kmod-nvidia-open nvidia-settings
         sudo akmods --rebuild --force
@@ -12,32 +12,17 @@ install_nvidia(){
         #xdg-open https://rpmfusion.org/Howto/NVIDIA?highlight=%28%5CbCategoryHowto%5Cb%29#Installing_the_drivers
     elif [ "$DISTRO" == "opensuse-tumbleweed" ] || [ "$DISTRO" == "opensuse-slowroll" ]
     then
-        ## this is for enabling the official repos and closed drivers from nvidia by installing the patterns 
-        # package. Packages here require accepting a license agreement.
+        #this is for enabling the official repos and closed drivers from nvidia by installing the patterns 
+        #package. Packages here require accepting a license agreement.
 
         #sudo zypper -n install openSUSE-repos-Tumbleweed-NVIDIA
+        #sudo zypper -n install openSUSE-repos-Slowroll-NVIDIA
         #sudo zypper --gpg-auto-import-keys ref
         #sudo zypper -n install --auto-agree-with-licenses nvidia-video-G06 nvidia-settings nvidia-modprobe
 
 
         #xdg-open "$SCRIPTS_FOLDER"/modules/packages/hardware/nvidia.txt
         #xdg-open "https://en.opensuse.org/SDB:NVIDIA_drivers#Via_YaST_(for_Leap_and_Tumbleweed)"
-
-        # some weird bug MicroOS patterns are selected by default on tumbleweed. To be safe remove/lock them all.
-        sudo zypper rm openSUSE-repos-MicroOS-NVIDIA openSUSE-repos-Tumbleweed-NVIDIA openSUSE-repos-Slowroll-NVIDIA
-        sudo zypper al openSUSE-repos-MicroOS-NVIDIA openSUSE-repos-Tumbleweed-NVIDIA openSUSE-repos-Slowroll-NVIDIA
-
-        # also install module for lts kernel support for safety.
-        sudo zypper -n install nvidia-open-driver-G06-signed-kmp-default nvidia-open-driver-G06-signed-kmp-longterm\
-        nvidia-settings nvidia-modprobe
-    elif [ "$DISTRO" == "opensuse-slowroll" ]
-    then
-        ## this is for enabling the official repos and closed drivers from nvidia by installing the patterns 
-        # package. Packages here require accepting a license agreement.
-        #sudo zypper -n install openSUSE-repos-Slowroll-NVIDIA
-        #sudo zypper --gpg-auto-import-keys ref
-        #sudo zypper -n install --auto-agree-with-licenses nvidia-video-G06 nvidia-settings nvidia-modprobe
-
 
         # some weird bug MicroOS patterns are selected by default on tumbleweed. To be safe remove/lock them all.
         sudo zypper rm openSUSE-repos-MicroOS-NVIDIA openSUSE-repos-Tumbleweed-NVIDIA openSUSE-repos-Slowroll-NVIDIA
