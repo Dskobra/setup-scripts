60764c9832b6c5d294a2d6fea597fda9d9f68e92
Jordan Bottoms <jbttoms@gmail.com>
2025-03-15 23:18:37 -0500

    Update prereq.sh
    added new prereq checks

--- modules/core/prereq.sh
+++ modules/core/prereq.sh
@@ -20,12 +20,49 @@ install_prereq(){
     flatpak install --user -y flathub com.github.tchx84.Flatseal        # allows easily managing permissions for flatpaks
 }
 
+wget_check(){
+    if test -f /usr/bin/wget; then
+        echo "Wget already installed."
+    elif ! test -f /usr/bin/wget; then
+        PACKAGES_TO_INSTALL+=" wget"
+    fi
+}
+
+curl_check(){
+    if test -f /usr/bin/curl; then
+        echo "Curl already installed."
+    elif ! test -f /usr/bin/curl; then
+        PACKAGES_TO_INSTALL+=" curl"
+    fi
+}
+
+flatpak_check(){
+    if test -f /usr/bin/flatpak; then
+        echo "flatpak already installed."
+    elif ! test -f /usr/bin/flatpak; then
+        PACKAGES_TO_INSTALL+=" flatpak"
+
+    fi
+}
+
+dnf_plugins_core_check(){
+    if test -f /etc/dnf/plugins/copr.conf; then
+        echo "dnf-plugins-core already installed."
+    elif ! test -f /etc/dnf/plugins/copr.conf; then
+        PACKAGES_TO_INSTALL+=" dnf-plugins-core"
+    fi
+}
+
 run_prereq_check(){
     ### check if required packages should be installed.
     ### 0 (default) for yes and 1 for no
     PREREQ_FILE=$(cat $SCRIPTS_FOLDER/.prereq.txt)
    if [ "$PREREQ_FILE" != "1" ]
     then
+        wget_check
+        curl_check
+        flatpak_check
+        echo $PACKAGES_TO_INSTALL
         mkdir /home/$USER/bin
         sudo mkdir /opt/apps/
         sudo mkdir /opt/apps/icons
@@ -38,4 +75,6 @@ run_prereq_check(){
         echo "Skipping first run steps."
     fi
 }
+
+PACKAGES_TO_INSTALL=""
 run_prereq_check
