d58e01e2d3adcf1a30d87d1bf0b856ee13a5e6d7
Jordan Bottoms <jbttoms@gmail.com>
2025-03-19 22:59:07 -0500

    Update codecs.sh
    Remove vendor change to avoid swapping entire mesa stack, pipewire etc. Also removed old fedora warning. Codecs should be downgraded and scripts no longer support atomic (which was its initial purpose)

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -6,7 +6,6 @@ native_codecs(){
         sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
         sudo dnf install -y ffmpeg ffmpeg-libs.i686 ffmpeg-libs
 
-        mesa_fedora_warning
         sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
         sudo dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 
@@ -17,26 +16,14 @@ native_codecs(){
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
-        sudo zypper dup --from packman-essentials --allow-vendor-change
     elif [ "$DISTRO" == "opensuse-slowroll" ]
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper  install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
-        sudo zypper dup --from packman-essentials --allow-vendor-change
     else
         echo "Unkown error has occurred."
     fi
-}
-
-mesa_fedora_warning(){
-    echo "=============================================================="
-    echo "If you receive installation errors you will likely have to"
-    echo "wait a few days to try again. This is due to needing to"
-    echo "rebuild against the updated Mesa and lack of collaboration"
-    echo "between Fedora package maintainers and RPMFusion which leaves"
-    echo "them broken on updates."
-    echo "=============================================================="
 }
 
 native_codecs
