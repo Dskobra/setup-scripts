d473ccb2d7c1fec8e11504d02b0d0243c01709e5
Jordan Bottoms <jbttoms@gmail.com>
2025-03-22 00:15:08 -0500

    Update codecs.sh
    No longer do vendor change with packman

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -10,20 +10,11 @@ native_codecs(){
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
-
-        sudo zypper al Mesa Mesa-dri Mesa-gallium Mesa-libEGL1 Mesa-libGL1 Mesa-libva libgbm1
-        sudo zypper -n dup --from packman-essentials --allow-vendor-change
-        sudo zypper rl Mesa Mesa-dri Mesa-gallium Mesa-libEGL1 Mesa-libGL1 Mesa-libva libgbm1
     elif [ "$DISTRO" == "opensuse-slowroll" ]
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
-
-        sudo zypper al Mesa Mesa-dri Mesa-gallium Mesa-libEGL1 Mesa-libGL1 Mesa-libva libgbm1
-        sudo zypper -n dup --from packman-essentials --allow-vendor-change
-        sudo zypper rl Mesa Mesa-dri Mesa-gallium Mesa-libEGL1 Mesa-libGL1 Mesa-libva libgbm1
-
     else
         echo "Unkown error has occurred."
     fi
