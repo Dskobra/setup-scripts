10487d67739b3495e625a708af15c2e66c8f01cb
Jordan Bottoms <jbttoms@gmail.com>
2025-03-23 06:56:56 -0500

    Update codecs.sh
    Reverted codec changes

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -5,19 +5,28 @@ native_codecs(){
     then
         sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
         sudo dnf install -y ffmpeg ffmpeg-libs.i686 ffmpeg-libs
+
+        sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
+        sudo dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+
+        sudo dnf swap -y mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+        sudo dnf swap -y mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
     elif [ "$DISTRO" == "opensuse-tumbleweed" ]
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
+        sudo zypper dup --from packman-essentials --allow-vendor-change
     elif [ "$DISTRO" == "opensuse-slowroll" ]
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
+        sudo zypper dup --from packman-essentials --allow-vendor-change
     else
         echo "Unkown error has occurred."
     fi
 }
-echo "Amd hardware video acceleration won't be installed."
+# personal note idk if it's switching mesa vendor to packman thats creating system instability on my pc or some other issue.
+# and frankly tired of manually selecting packages to change vendors. So just change mesa and codecs etc....
 native_codecs
