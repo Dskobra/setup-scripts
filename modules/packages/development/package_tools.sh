a98ea6c01f67f12b793ab7ddb09ca2d4197b4eaf
Jordan Bottoms <jbttoms@gmail.com>
2025-03-23 12:32:22 -0500

    Update package_tools.sh
    change to using patterns/groups

--- modules/packages/development/package_tools.sh
+++ modules/packages/development/package_tools.sh
@@ -3,14 +3,11 @@
 native_package_tools(){
     if [ "$DISTRO" == "fedora" ]
     then
-        sudo dnf install -y gcc-g++ autoconf automake bison flex libtool\
-        m4 valgrind byacc ccache cscope indent ltrace perf strace koji\
-        mock redhat-rpm-config rpm-build rpmdevtools
+        sudo dnf4 groupinstall -y "Development Tools"
+        sudo dnf4 groupinstall -y "C Development Tools and Libraries"
     elif [ "$DISTRO" == "opensuse-tumbleweed" ] || [ "$DISTRO" == "opensuse-slowroll" ]
     then
-        sudo zypper -n install gcc-c++ autoconf automake bison flex libtool\
-        m4 valgrind byacc ccache cscope indent ltrace perf strace rpm-build\
-        build inst-source-utils
+        sudo zypper -n install patterns-devel-C-C++-devel_C_C++
     else
         echo "Unkown error has occurred."
     fi
