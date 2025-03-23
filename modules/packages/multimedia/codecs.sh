cbbbd38b4521882f2b497580ef1c25981be84082
Jordan Bottoms <jbttoms@gmail.com>
2025-03-23 07:04:06 -0500

    Update codecs.sh
    readded auto accept

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -16,13 +16,13 @@ native_codecs(){
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
-        sudo zypper dup --from packman-essentials --allow-vendor-change
+        sudo zypper -n dup --from packman-essentials --allow-vendor-change
     elif [ "$DISTRO" == "opensuse-slowroll" ]
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
-        sudo zypper dup --from packman-essentials --allow-vendor-change
+        sudo zypper -n dup --from packman-essentials --allow-vendor-change
     else
         echo "Unkown error has occurred."
     fi
