59ec7a32806c03eaf49031015cbed6c60deb07f6
Jordan Bottoms <jbttoms@gmail.com>
2025-03-29 20:52:13 -0500

    Update setup.sh
    version bump

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@
 export SCRIPTS_FOLDER                           # stores full path for setup-scripts
 export DISTRO=""                                # stores distro name.
 export COPYRIGHT="Copyright (c) 2021-2025 Jordan Bottoms"
-export VERSION="3.27.2025"
+export VERSION="3.29.2025"
 SCRIPTS_FOLDER=$(pwd)                     
 "$SCRIPTS_FOLDER"/modules/core/launcher.sh
