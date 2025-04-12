44fb26dc9e98dbc75cc68a60912fbe861d07debb
Dskobra <jbttoms@gmail.com>
2025-04-12 18:40:34 -0500

    now marks codecs as installed after run so future packages that require them wont install them

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -16,28 +16,26 @@ native_codecs(){
         
         sudo dnf swap -y mesa-vulkan-drivers mesa-vulkan-drivers-freeworld
         sudo dnf swap -y mesa-vulkan-drivers.i686 mesa-vulkan-drivers-freeworld.
-        echo "1" > "$SCRIPTS_FOLDER"/modules/multimedia/codecs.log
+        echo "1" > "$SCRIPTS_FOLDER"/modules/multimedia/codecs.txt
     elif [ "$DISTRO" == "opensuse-slowroll" ]
     then
         sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/' packman-essentials
         sudo zypper --gpg-auto-import-keys ref
         sudo zypper -n install --from packman-essentials ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec
         sudo zypper -n dup --from packman-essentials --allow-vendor-change
+        echo "1" > "$SCRIPTS_FOLDER"/modules/multimedia/codecs.txt
     else
         echo "Unkown error has occurred."
     fi
 }
 
-native_codecs
-
-codecs_installed=""
+CODECS_INSTALLED="$(cat < "$SCRIPTS_FOLDER"/modules/packages/multimedia/codecs.txt)"
 
 
-if [ "$VERSION_ID" == "41" ] && [ -z "$VARIANT" ] || [ "$VERSION_ID" == "42" ] && [ -z "$VARIANT" ]
+if [ "$CODECS_INSTALLED" == "0" ]
 then
-    "$SCRIPTS_FOLDER"/modules/core/prereq.sh
-    "$SCRIPTS_FOLDER"/modules/core/menu.sh
+    native_codecs
 else
-    echo "These scripts only support Fedora 41/42 non atomic editions."
+    echo "Codecs already installed."
 fi
 "$SCRIPTS_FOLDER"/modules/core/launcher.sh
