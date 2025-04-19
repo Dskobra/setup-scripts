0e307efcf0939e8ea9280d979282ebd4876efcdd
Dskobra <jbttoms@gmail.com>
2025-04-18 23:08:04 -0500

    removed nvidia open driver (will readd once they make it the default)

--- modules/packages/hardware/nvidia.sh
+++ modules/packages/hardware/nvidia.sh
@@ -3,8 +3,7 @@
 install_nvidia(){
     if [ "$DISTRO" == "fedora" ]
     then
-        sudo dnf install -y rpmfusion-nonfree-release-tainted
-        sudo dnf install -y akmod-nvidia-open kmod-nvidia-open nvidia-settings
+        sudo dnf install -y akmod-nvidia nvidia-settings
         sudo akmods --rebuild --force
         sudo dracut --regenerate-all --force
     elif [ "$DISTRO" == "opensuse-slowroll" ]
