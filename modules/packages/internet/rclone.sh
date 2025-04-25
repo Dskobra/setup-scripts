34b16b044c7bcd19bc2214106f9b2089d7eb0dce
Dskobra <jbttoms@gmail.com>
2025-04-24 23:22:29 -0500

    use fedora provided rclone

--- modules/packages/internet/rclone.sh
+++ modules/packages/internet/rclone.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/bash
-
-install_rclone(){
-    #sudo -v ; curl https://rclone.org/install.sh | sudo bash
-
-    RCLONE_BROWSER_LINK="https://github.com/kapitainsky/RcloneBrowser/releases/download/1.8.0/rclone-browser-1.8.0-a0b66c6-linux-x86_64.AppImage"
-    RCLONE_BROWSER_BINARY="rclone-browser-1.8.0-a0b66c6-linux-x86_64.AppImage"
-    
-    if test -f /opt/apps/appimages/"$RCLONE_BROWSER_BINARY"; then
-        echo "Rclone Browser already downloaded."
-    elif ! test -f /opt/apps/appimages/"$RCLONE_BROWSER_BINARY"; then
-        cd /opt/apps/appimages/ || exit
-        curl -L -o "$RCLONE_BROWSER_BINARY" "$RCLONE_BROWSER_LINK"
-        chmod +x "$RCLONE_BROWSER_BINARY"
-
-        curl -L -o $SCRIPTS_FOLDER/temp/rclone_browser.png https://github.com/kapitainsky/RcloneBrowser/blob/master/assets/rclone-browser-32x32.png?raw=true
-        mv $SCRIPTS_FOLDER/temp/rclone_browser.png /opt/apps/icons
-
-        cp $SCRIPTS_FOLDER/modules/data/rclone_browser.sh /home/$USER/bin/rclone_browser
-        cp $SCRIPTS_FOLDER/modules/data/rclone_browser.desktop /home/$USER/Desktop/rclone_browser.desktop
+native_rclone(){
+    if [ "$DISTRO" == "fedora" ]
+    then
+        sudo dnf install -y rclone rclone-browser
+    else
+        echo "Unkown error has occurred."
     fi
-
-    sudo -v ; curl https://rclone.org/install.sh | sudo bash
-
 }
 
-install_rclone
+native_rclone()
