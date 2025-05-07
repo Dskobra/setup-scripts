3328ea9ab939643bbb7bd272f03ed7e65098a184
Dskobra <jbttoms@gmail.com>
2025-05-07 00:54:08 -0500

    export VERSION_ID

--- modules/packages/internet/mega.sh
+++ modules/packages/internet/mega.sh
@@ -4,12 +4,12 @@ native_mega(){
     if [ "$VERSION_ID" == "41" ]
     then
         cd /opt/apps/temp/
-        wget https://mega.nz/linux/repo/Fedora_41/x86_64/megasync-Fedora_41.x86_64.rpm && sudo dnf install "$PWD/megasync-Fedora_41.x86_64.rpm"
+        wget https://mega.nz/linux/repo/Fedora_41/x86_64/megasync-Fedora_41.x86_64.rpm && sudo dnf install -y "$PWD/megasync-Fedora_41.x86_64.rpm"
         rm megasync-Fedora_41.x86_64.rpm
     elif [ "$VERSION_ID" == "42" ]
     then
         cd /opt/apps/temp/
-        wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && sudo dnf install "$PWD/megasync-Fedora_42.x86_64.rpm"
+        wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && sudo dnf install -y "$PWD/megasync-Fedora_42.x86_64.rpm"
         rm megasync-Fedora_42.x86_64.rpm
     else
         echo "Unkown error has occurred."
--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 
 export SCRIPTS_FOLDER                           # stores full path for setup-scripts
 export DISTRO=""                                # stores distro name.
+export VERSION_ID=""
 export COPYRIGHT="Copyright (c) 2021-2025 Jordan Bottoms"
 export VERSION="4.28.2025.1"
 SCRIPTS_FOLDER=$(pwd)                     
