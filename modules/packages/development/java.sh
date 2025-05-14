44f6e39a0c8972e1391677988027d154fa03cc24
Dskobra <jbttoms@gmail.com>
2025-05-14 18:59:15 -0500

    fixed file name typo

--- modules/packages/development/java.sh
+++ modules/packages/development/java.sh
@@ -3,7 +3,7 @@ native_jdk(){
     if [ "$DISTRO" == "fedora" ]
     then
         sudo dnf install -y adoptium-temurin-java-repository
-        sudo sed -i '/enabled=0/c enabled=1' /etc/yum.repos.d/adpotium-temurin-java-repository.repo
+        sudo sed -i '/enabled=0/c enabled=1' /etc/yum.repos.d/adoptium-temurin-java-repository.repo
         sudo dnf update -y
         sudo dnf install -y temurin-21-jdk
         sudo alternatives --set java /usr/lib/jvm/temurin-21-jdk/bin/java
