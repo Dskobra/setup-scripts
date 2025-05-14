1a4dba34c384d534c6193c447b3cfdf48de0df37
Dskobra <jbttoms@gmail.com>
2025-05-13 20:00:28 -0500

    added back asking to choose package type on selection

--- modules/packages/extras/gtkhash.sh
+++ modules/packages/extras/gtkhash.sh
@@ -18,6 +18,26 @@ remove_gtkhash(){
     fi
 }
 
+
+package_chooser(){
+    echo "Choose package type:"
+    echo ""
+    echo "1: flatpak 2: native"
+    read -r input
+    read input
+    if [ "$input" == "1" ] || [ -z "$input" ]
+    then
+        flatpak install --user -y flathub org.gtkhash.gtkhash
+        remove_gtkhash
+    elif [ "$input" == "2" ]
+    then
+        flatpak remove --user -y org.gtkhash.gtkhash
+        native_gtkhash
+    else
+        echo ""
+    fi
+}
+
 if [ "$1" == "flatpak" ]
 then
     flatpak install --user -y flathub org.gtkhash.gtkhash
