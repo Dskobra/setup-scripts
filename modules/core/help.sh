5ce637723a6d3c009e5b88ea44a0a12f6ba5fe1b
Dskobra <jbttoms@gmail.com>
2025-05-14 01:09:27 -0500

    added help section

---
+++ modules/core/help.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+help(){
+    echo "//native//               These are applications that are built for Fedora in an RPM format. Normally they perform better and consume less space."
+    echo "                         Generally recommended for most applications (except audio/video codecs as they involve more install steps and may"
+    echo "                         break on updates)"
+    echo ""
+    echo ""
+    echo "//flatpak//              These are applications that are built for all distros using a neutral format. Some developers consider flatpaks their"
+    echo "                         officially supported release. Automatically handles installing required components and audio/video codecs at the cost"
+    echo "                         of more space. Generally recommended for applications involving audio/video codecs or when officially supported."
+}
--- modules/packages/extras/gtkhash.sh
+++ modules/packages/extras/gtkhash.sh
@@ -20,18 +20,25 @@ remove_gtkhash(){
 
 
 package_chooser(){
-    echo "Choose package type:"
-    echo ""
-    echo "1: flatpak 2: native"
-    read -r input
-    if [ "$input" == "1" ] || [ -z "$input" ]
-    then
-        flatpak install --user -y flathub org.gtkhash.gtkhash
-        remove_gtkhash
-    elif [ "$input" == "2" ]
+    echo "Select the type of package to install."
+    echo "Enter an option or leave blank for default"
+    echo "(1) Native                (2) Flatpak(default)"
+    echo "(3) Help                  (3) Cancel"
+    read -r PACKAGE_TYPE
+    if [ "$PACKAGE_TYPE" == "1" ]
     then
         flatpak remove --user -y org.gtkhash.gtkhash
         native_gtkhash
+    elif [ "$PACKAGE_TYPE" == "2" ] || [ -z "$PACKAGE_TYPE" ]
+    then
+        flatpak install --user -y flathub org.gtkhash.gtkhash
+        remove_gtkhash
+    elif [ "$PACKAGE_TYPE" == "3" ]
+    then
+        "$SCRIPTS_FOLDER"/modules/core/help.sh
+    elif [ "$PACKAGE_TYPE" == "0" ]
+    then
+        echo "User canceled"
     else
         echo "Unkown error has occurred."
     fi
