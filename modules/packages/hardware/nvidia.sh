d745c3045d832cc24e657a310bd1f48d793cf2f9
Dskobra <jbttoms@gmail.com>
2025-05-16 18:22:28 -0500

    readded open kernel driver

--- modules/packages/hardware/nvidia.sh
+++ modules/packages/hardware/nvidia.sh
@@ -3,9 +3,17 @@
 install_nvidia(){
     if [ "$DISTRO" == "fedora" ]
     then
-        sudo dnf install -y akmod-nvidia nvidia-settings
+        sudo dnf install -y akmod-nvidia nvidia-settings\
+        xorg-x11-drv-nvidia-cuda
         sudo akmods --rebuild --force
         sudo dracut --regenerate-all --force
+
+        #sudo dnf install rpmfusion-nonfree-release-tainted
+        #sudo sh -c 'echo "%_with_kmod_nvidia_open 1" > /etc/rpm/macros.nvidia-kmod'
+        #sudo dnf swap akmod-nvidia akmod-nvidia-open
+        #sudo akmods --rebuild --force
+        #sudo dracut --regenerate-all --force
+
     else
         echo "Unkown error has occurred."
     fi
