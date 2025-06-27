d704ec32c54077efb6bdb84c4d6101f1578fa685
Dskobra <jbttoms@gmail.com>
2025-06-27 03:44:06 -0500

    fixed gaming tools path

--- modules/packages/fedora/gaming/steam.sh
+++ modules/packages/fedora/gaming/steam.sh
@@ -9,11 +9,11 @@ package_chooser(){
     if [ "$PACKAGE_TYPE" == "1" ] || [ -z "$PACKAGE_TYPE" ]
     then
         flatpak remove --user -y com.valvesoftware.Steam
-        "$SCRIPTS_FOLDER"/modules/packages/gaming/game_tools.sh "native"
+        "$SCRIPTS_FOLDER"/modules/packages/fedora/gaming/game_tools.sh "native"
         sudo dnf install -y steam
     elif [ "$PACKAGE_TYPE" == "2" ]
     then
-        "$SCRIPTS_FOLDER"/modules/packages/gaming/game_tools.sh "flatpak"
+        "$SCRIPTS_FOLDER"/modules/packages/fedora/gaming/game_tools.sh "flatpak"
         echo "gamemode and steam-devices package will also be installed for performance optimization"
         echo "and controller support."
         flatpak install --user -y flathub com.valvesoftware.Steam
