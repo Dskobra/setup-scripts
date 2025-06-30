d3bb71d229cf377ae9da123e7a4fb848d016232a
Dskobra <jbttoms@gmail.com>
2025-06-29 19:50:51 -0500

    cleanup

--- modules/packages/fedora/development/java.sh
+++ modules/packages/fedora/development/java.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/bash
-install_openjdk(){
-    sudo dnf install -y adoptium-temurin-java-repository
-    sudo sed -i '/enabled=0/c enabled=1' /etc/yum.repos.d/adoptium-temurin-java-repository.repo
-    sudo dnf update -y
-    sudo dnf install -y temurin-21-jdk
-    sudo alternatives --set java /usr/lib/jvm/temurin-21-jdk/bin/java
-}
 
-install_openjdk
+sudo dnf install -y adoptium-temurin-java-repository
+sudo sed -i '/enabled=0/c enabled=1' /etc/yum.repos.d/adoptium-temurin-java-repository.repo
+sudo dnf update -y
+sudo dnf install -y temurin-21-jdk
+sudo alternatives --set java /usr/lib/jvm/temurin-21-jdk/bin/java
