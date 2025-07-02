85af6e5b78fba79a24b8114bdf139eb71f97bdaf
Dskobra <jbttoms@gmail.com>
2025-07-02 01:24:53 -0500

    fixed missing space
    fixed missing space for multiline command

--- modules/packages/fedora/development/virtualization.sh
+++ modules/packages/fedora/development/virtualization.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 sudo dnf install -y libvirt-daemon-config-network libvirt-daemon-kvm\
-qemu-kvm virt-install virt-manager virt-viewer
+ qemu-kvm virt-install virt-manager virt-viewer
 sudo usermod -aG libvirt "$USER"
 xdg-open "https://github.com/virtio-win/virtio-win-pkg-scripts/blob/master/README.md"
