cc058429449c1fb6532e373deb7145f567f876f0
Jordan Bottoms <jbttoms@gmail.com>
2025-07-23 05:41:50 -0500

    cleaned up nvidia code
    Meta package (nvidia-open-driver-G06-signed-kmp-meta) will install most
    things excluding current kernel (only longterm for some reason)

--- modules/packages/opensuse-slowroll/hardware/nvidia.sh
+++ modules/packages/opensuse-slowroll/hardware/nvidia.sh
@@ -4,9 +4,8 @@ install_nvidia(){
     sudo zypper -n  install openSUSE-repos-Slowroll-NVIDIA
     sudo zypper --gpg-auto-import-keys ref
     sudo zypper -n dup --auto-agree-with-licenses
-    sudo zypper -n install --auto-agree-with-licenses nvidia-open-driver-G06-signed-kmp-meta nvidia-open-driver-G06-signed-kmp-longterm \
-    nvidia-video-G06 nvidia-userspace-meta-G06 nvidia-persistenced nvidia-modprobe nvidia-gl-G06 nvidia-compute-utils-G06 \
-    nvidia-compute-G06 nvidia-common-G06 libnvidia-egl-x111 libnvidia-egl-wayland1 libnvidia-egl-gbm1 libOpenCL1 clinfo nvidia-settings
+    sudo zypper -n install --auto-agree-with-licenses nvidia-open-driver-G06-signed-kmp-meta nvidia-open-driver-G06-signed-kmp-default \
+    nvidia-open-driver-G06-signed-kmp-longterm nvidia-settings
     sudo dracut --regenerate-all --force
 }
 
