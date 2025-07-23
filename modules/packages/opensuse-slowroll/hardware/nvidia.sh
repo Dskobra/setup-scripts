2820d98b212280d53806199265ac67ed5cb24b7e
Jordan Bottoms <jbttoms@gmail.com>
2025-07-22 23:18:02 -0500

    install nvidia patterns
    Seems only yast auto installs nvidia patterns. So just install them
    anyway

--- modules/packages/opensuse-slowroll/hardware/nvidia.sh
+++ modules/packages/opensuse-slowroll/hardware/nvidia.sh
@@ -5,11 +5,16 @@ install_nvidia(){
     #package. Packages here require accepting a license agreement.
 
     # run zypper dup first as it should auto install the nvidia open kernel driver
+    sudo zypper -n  install openSUSE-repos-Slowroll-NVIDIA
     sudo zypper -n dup
     sudo zypper --gpg-auto-import-keys ref
     # also install lts kernel driver as backup
-    sudo zypper -n install --auto-agree-with-licenses nvidia-video-G06 nvidia-settings nvidia-modprobe\
-    nvidia-userspace-meta-G06  nvidia-open-driver-G06-signed-kmp-longterm
+    sudo zypper -n install --auto-agree-with-licenses nvidia-video-G06 nvidia-settings nvidia-modprobe \
+    nvidia-userspace-meta-G06 nvidia-open-driver-G06-signed-kmp-longterm libnvidia-egl-x111-32bit \
+    libnvidia-egl-wayland1-32bit libnvidia-egl-gbm1-32bit
+    sudo zypper install-new-recommends --repo repo-non-free
 }
 
 install_nvidia
+
+
