668c287dac2ddbce87eedcf2e81e5c3b9d165810
Jordan Bottoms <jbttoms@gmail.com>
2025-08-02 22:22:34 -0500

    fixed another nvidia issue

--- modules/packages/opensuse/hardware/nvidia.sh
+++ modules/packages/opensuse/hardware/nvidia.sh
@@ -10,8 +10,11 @@ install_nvidia(){
     else
         echo "Error has occured."
     fi
+    # Currently opensuse auto installs on fresh installs the nvidia-open-driver-G06-signed-kmp-meta package which
+    # is out of date and prevents the rest of nvidia packages from installing. So clear nvidia packages out first.
+    sudo zypper -n rm nvidia*
     # note on tumbleweed it likes to add MicroOS repos for nvidia for some reason. Slowroll
-    # also had a bug in july wehre it added leap repos. So just put a lock.
+    # also had a bug in july where it added leap repos. So just put a lock.
     sudo zypper al openSUSE-repos-MicroOS-NVIDIA openSUSE-repos-Leap-NVIDIA openSUSE-repos-MicroOS
     sudo zypper --gpg-auto-import-keys ref
     sudo zypper -n dup --auto-agree-with-licenses
