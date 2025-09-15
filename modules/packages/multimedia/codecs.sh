bf4253d65e2dafd7661214d1662b02e3fb428ab1
Dskobra <jbttoms@protonmail.com>
2025-09-15 03:16:58 -0500

    Removed vdpau
    also added early fedora 43 support

--- modules/packages/multimedia/codecs.sh
+++ modules/packages/multimedia/codecs.sh
@@ -17,16 +17,12 @@ amd_gpu_codecs(){
         sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
         sudo dnf swap -y mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
 
-        sudo dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
-        sudo dnf swap -y mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
-
-
         sudo dnf swap -y mesa-vulkan-drivers mesa-vulkan-drivers-freeworld
         sudo dnf swap -y mesa-vulkan-drivers.i686 mesa-vulkan-drivers-freeworld.i686
         echo "1" > "$SCRIPTS_FOLDER"/modules/packages/multimedia/codecs.txt
 
         rm "$SCRIPTS_FOLDER"/modules/packages/mesa_versions.txt
-        { rpm -q mesa-va-drivers-freeworld; rpm -q mesa-vdpau-drivers-freeworld; rpm -q mesa-vulkan-drivers-freeworld; } >> "$SCRIPTS_FOLDER"/modules/packages/mesa_versions.txt
+        { rpm -q mesa-va-drivers-freeworld; rpm -q mesa-vulkan-drivers-freeworld; } >> "$SCRIPTS_FOLDER"/modules/packages/mesa_versions.txt
     else
         echo "Unkown error has occurred."
     fi
@@ -41,10 +37,6 @@ remove_codecs(){
 
         sudo dnf swap -y mesa-va-drivers-freeworld mesa-va-drivers
         sudo dnf swap -y mesa-va-drivers-freeworld.i686 mesa-va-drivers.i686
-
-        sudo dnf swap -y mesa-vdpau-drivers-freeworld mesa-vdpau-drivers
-        sudo dnf swap -y mesa-vdpau-drivers-freeworld.i686 mesa-vdpau-drivers.i686
-
 
         sudo dnf swap -y mesa-vulkan-drivers-freeworld mesa-vulkan-drivers
         sudo dnf swap -y mesa-vulkan-drivers-freeworld.i686 mesa-vulkan-drivers.i686
